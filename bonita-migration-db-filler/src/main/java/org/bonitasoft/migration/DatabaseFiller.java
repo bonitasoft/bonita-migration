e158e1152e2beb7723348af6633e46ca4d76eb94
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-11-08 17:31:30 +0100

    Add test execution with bonita home retrieval

--- bonita-migration-db-filler/src/main/java/org/bonitasoft/migration/DatabaseFiller.java
+++ bonita-migration-db-filler/src/main/java/org/bonitasoft/migration/DatabaseFiller.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright (C) 2013 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
 package org.bonitasoft.migration;
 
 import java.io.InputStream;
@@ -5,7 +18,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.bonitasoft.engine.TestsInitializer;
 import org.bonitasoft.engine.api.IdentityAPI;
 import org.bonitasoft.engine.api.TenantAPIAccessor;
 import org.bonitasoft.engine.io.IOUtil;
@@ -25,7 +37,7 @@ public class DatabaseFiller {
 
     private void execute() throws Exception {
         logger.info("Using bonita.home: " + System.getProperty("bonita.home"));
-        TestsInitializer.beforeAll();
+        // TestsInitializer.beforeAll();
         logger.info("Starting to fill the database");
         APISession session = APITestUtil.loginDefaultTenant();
         ArrayList<String> stats = new ArrayList<String>();
---
+++ bonita-migration-db-filler/src/test/java/org/bonitasoft/migration/DatabaseFillerTest.java
@@ -0,0 +1,12 @@
+package org.bonitasoft.migration;
+
+import org.junit.Test;
+
+public class DatabaseFillerTest {
+
+    @Test
+    public void testMain() throws Exception {
+        DatabaseFiller.main(null);
+    }
+
+}
