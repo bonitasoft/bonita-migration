7e9d2b953493aeba91fbce45c4eaa979f2fe9d9a
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-11-12 10:54:57 +0100

    add first working version of database filler

--- bonita-migration-db-filler/src/main/java/org/bonitasoft/migration/DatabaseFiller.java
+++ bonita-migration-db-filler/src/main/java/org/bonitasoft/migration/DatabaseFiller.java
@@ -13,18 +13,29 @@
  **/
 package org.bonitasoft.migration;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
+import javax.naming.Context;
+
 import org.bonitasoft.engine.api.IdentityAPI;
+import org.bonitasoft.engine.api.ProcessAPI;
 import org.bonitasoft.engine.api.TenantAPIAccessor;
+import org.bonitasoft.engine.bpm.bar.BusinessArchiveBuilder;
+import org.bonitasoft.engine.bpm.process.ProcessDefinition;
+import org.bonitasoft.engine.bpm.process.impl.ProcessDefinitionBuilder;
+import org.bonitasoft.engine.exception.BonitaException;
 import org.bonitasoft.engine.io.IOUtil;
 import org.bonitasoft.engine.session.APISession;
 import org.bonitasoft.engine.test.APITestUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DatabaseFiller {
 
@@ -38,10 +49,12 @@ public class DatabaseFiller {
     private void execute() throws Exception {
         logger.info("Using bonita.home: " + System.getProperty("bonita.home"));
         // TestsInitializer.beforeAll();
+        setup();
         logger.info("Starting to fill the database");
         APISession session = APITestUtil.loginDefaultTenant();
         ArrayList<String> stats = new ArrayList<String>();
         stats.addAll(fillOrganization(session));
+        stats.addAll(fillProcesses(session));
         APITestUtil.logoutTenant(session);
         logger.info("Finished to fill the database");
         for (String string : stats) {
@@ -51,12 +64,45 @@ public class DatabaseFiller {
 
     }
 
+    private Collection<? extends String> fillProcesses(final APISession session) throws Exception {
+        ProcessAPI processAPI = TenantAPIAccessor.getProcessAPI(session);
+        IdentityAPI identityAPI = TenantAPIAccessor.getIdentityAPI(session);
+
+        ProcessDefinitionBuilder builder = new ProcessDefinitionBuilder().createNewInstance("MyProcess", "1.0");
+        builder.addActor("theActor");
+        builder.addUserTask("step1", "theActor");
+        BusinessArchiveBuilder archiveBuilder = new BusinessArchiveBuilder().createNewBusinessArchive().createNewBusinessArchive();
+        archiveBuilder.setProcessDefinition(builder.done());
+        ProcessDefinition processDefinition = processAPI.deploy(archiveBuilder.done());
+        processAPI.addUserToActor("theActor", processDefinition, identityAPI.getUserByUserName("william.jobs").getId());
+        processAPI.enableProcess(processDefinition.getId());
+        processAPI.startProcess(processDefinition.getId());
+        processAPI.startProcess(processDefinition.getId());
+        processAPI.startProcess(processDefinition.getId());
+        return Arrays.asList("definitions: " + 1, "instances: " + 3);
+    }
+
     private List<String> fillOrganization(final APISession session) throws Exception {
         IdentityAPI identityAPI = TenantAPIAccessor.getIdentityAPI(session);
         InputStream acme = this.getClass().getResourceAsStream("/org/bonitasoft/engine/identity/ACME.xml");
         identityAPI.importOrganization(IOUtil.read(acme));
         return Arrays.asList("Users: " + identityAPI.getNumberOfUsers(), "Groups: " + identityAPI.getNumberOfGroups(),
                 "Roles: " + identityAPI.getNumberOfRoles());
+    }
+
+    static ConfigurableApplicationContext springContext;
+
+    public void setup() throws BonitaException, IOException {
+        setSystemPropertyIfNotSet("sysprop.bonita.db.vendor", "h2");
+        // Force these system properties
+        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.bonitasoft.engine.local.SimpleMemoryContextFactory");
+        System.setProperty(Context.URL_PKG_PREFIXES, "org.bonitasoft.engine.local");
+        springContext = new ClassPathXmlApplicationContext("datasource.xml", "jndi-setup.xml");
+        APITestUtil.createInitializeAndStartPlatformWithDefaultTenant(false);
+    }
+
+    private static void setSystemPropertyIfNotSet(final String property, final String value) {
+        System.setProperty(property, System.getProperty(property, value));
     }
 
 }
