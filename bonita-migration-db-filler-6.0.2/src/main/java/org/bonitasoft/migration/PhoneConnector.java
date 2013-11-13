84f89e224157e684676e00ee5acf37455af295ea
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-11-13 17:46:35 +0100

    add more content in db filler

---
+++ bonita-migration-db-filler-6.0.2/src/main/java/org/bonitasoft/migration/PhoneConnector.java
@@ -0,0 +1,19 @@
+package org.bonitasoft.migration;
+
+import org.bonitasoft.engine.connector.AbstractConnector;
+import org.bonitasoft.engine.connector.ConnectorException;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+
+public class PhoneConnector extends AbstractConnector {
+
+    @Override
+    public void validateInputParameters() throws ConnectorValidationException {
+
+    }
+
+    @Override
+    protected void executeBusinessLogic() throws ConnectorException {
+
+    }
+
+}
