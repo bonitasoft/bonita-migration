0e99a4e6bd3d3700aba1f30184614226122ae1bc
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-11-13 18:01:18 +0100

    fix execute update for mysql

--- bonita-migration-distrib/src/main/groovy/org/bonitasoft/migration/versions/Version_6_0_2_to_6_1_0.groovy
+++ bonita-migration-distrib/src/main/groovy/org/bonitasoft/migration/versions/Version_6_0_2_to_6_1_0.groovy
@@ -95,10 +95,10 @@ public class Version_6_0_2_to_6_1_0 {
                 directoryId = row[0]
             }
             if(adminId != null && directoryId != null){
-                println sql.executeUpdate(new File(feature,dbVendor+"-update.sql").text
+                sql.execute(new File(feature,dbVendor+"-update.sql").text
                         .replaceAll(":tenantId", String.valueOf(it))
                         .replaceAll(":admin_profile_id", String.valueOf(adminId))
-                        .replaceAll(":dir_profile_entry_id", String.valueOf(directoryId))) + " row(s) updated";
+                        .replaceAll(":dir_profile_entry_id", String.valueOf(directoryId)));
             }
             println "done";
         }
