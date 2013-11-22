70a47d9689a5d184de3a5e502a7f1532549e2385
Céline Souchet <celine.souchet@bonitasoft.com>
2013-11-22 09:10:06 +0100

    Fix bad commit

---
+++ bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Bonita-home/MigrateClientBonitaHome.groovy
@@ -0,0 +1,48 @@
+import org.bonitasoft.migration.core.MigrationUtil;
+
+def currentDir = ""
+
+def File newClientBonitaHome = new File(feature, "bonita/client/");
+def File oldClientBonitaHome = new File(bonitaHome, "/client/");
+
+currentDir = "/platform/conf"
+MigrationUtil.migrateDirectory(newClientBonitaHome.path + currentDir, oldClientBonitaHome.path + currentDir)
+
+currentDir = "/platform/tenant-template"
+MigrationUtil.migrateDirectory(newClientBonitaHome.path + currentDir, oldClientBonitaHome.path + currentDir)
+
+currentDir = "/platform/work"
+MigrationUtil.migrateDirectory(newClientBonitaHome.path + currentDir, oldClientBonitaHome.path + currentDir)
+
+println "Detecting tenants..."
+def tenantsClientDir = new File(oldClientBonitaHome, "/tenants")
+if (tenantsClientDir.exists()) {
+    def tenants = Arrays.asList(tenantsServerDir.listFiles());
+    if (tenants.empty){
+        println "Not found any tenants."
+    } else {
+        println "Executing update for each tenant : " + tenants;
+        tenantsClientDir.eachFile { tenant ->
+            println "For tenant : " + tenant.name
+            PrintStream stdout = setSystemOutWithTab(5);
+
+            currentDir = "/conf"
+            MigrationUtil.migrateDirectory(newClientBonitaHome.path + "/platform/tenant-template" + currentDir, tenant.path + currentDir)
+
+            currentDir = "/work/icons/default"
+            MigrationUtil.migrateDirectory(newClientBonitaHome.path + "/platform/tenant-template" + currentDir, tenant.path + currentDir)
+
+            currentDir = "/work/icons/priority"
+            MigrationUtil.migrateDirectory(newClientBonitaHome.path + "/platform/tenant-template" + currentDir, tenant.path + currentDir)
+
+            currentDir = "/work/icons/profiles"
+            MigrationUtil.migrateDirectory(newClientBonitaHome.path + "/platform/tenant-template" + currentDir, tenant.path + currentDir)
+
+            currentDir = "/work/looknfeel"
+            MigrationUtil.migrateDirectory(newClientBonitaHome.path + "/platform/tenant-template" + currentDir, tenant.path + currentDir)
+            System.setOut(stdout);
+        }
+    }
+} else {
+    println "Not found any tenants."
+}
---
+++ bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Bonita-home/MigrateServerBonitaHome.groovy
@@ -0,0 +1,32 @@
+import org.bonitasoft.migration.core.MigrationUtil;
+
+def currentDir = ""
+
+def File newServerBonitaHome = new File(feature, "bonita/server/");
+def File oldServerBonitaHome = new File(bonitaHome, "/server/");
+
+currentDir = "/platform/conf"
+MigrationUtil.migrateDirectory(newServerBonitaHome.path + currentDir, oldServerBonitaHome.path + currentDir)
+
+currentDir = "/platform/tenant-template"
+MigrationUtil.migrateDirectory(newServerBonitaHome.path + currentDir, oldServerBonitaHome.path + currentDir)
+
+println "Detecting tenants..."
+def tenantsServerDir = new File(oldServerBonitaHome, "/tenants")
+if (tenantsServerDir.exists()) {
+    def tenants = Arrays.asList(tenantsServerDir.listFiles());
+    if (tenants.empty){
+        println "Not found any tenants."
+    } else {
+        println "Executing update for each tenant : " + tenants;
+        tenantsServerDir.eachFile { tenant ->
+            println "For tenant : " + tenant.name
+            PrintStream stdout = setSystemOutWithTab(5);
+            
+            MigrationUtil.migrateDirectory(newServerBonitaHome.path + "/platform/tenant-template/conf", tenant.path + "/conf")
+            System.setOut(stdout);
+        }
+    }
+} else {
+    println "Not found any tenants."
+}
