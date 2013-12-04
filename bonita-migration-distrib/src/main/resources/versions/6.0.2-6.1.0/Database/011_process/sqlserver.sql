bcb41e7cb4517a6f940bc2f3570ce36ea0b18330
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-12-04 16:56:33 +0100

    fix process migration: add default 0 with the not nullwq

--- bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Database/011_process/sqlserver.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Database/011_process/sqlserver.sql
@@ -12,7 +12,7 @@ ALTER TABLE arch_process_instance ADD startedByDelegate NUMERIC(19,0) NOT NULL
 
 UPDATE process_instance SET startedBy=0 WHERE startedBy IS NULL
 @@
-ALTER TABLE process_instance ALTER COLUMN startedBy NUMERIC(19,0) NOT NULL
+ALTER TABLE process_instance ALTER COLUMN startedBy NUMERIC(19,0) NOT NULL DEFAULT 0
 @@
-ALTER TABLE process_instance ADD startedByDelegate NUMERIC(19,0) NOT NULL
+ALTER TABLE process_instance ADD startedByDelegate NUMERIC(19,0) NOT NULL DEFAULT 0
 @@
