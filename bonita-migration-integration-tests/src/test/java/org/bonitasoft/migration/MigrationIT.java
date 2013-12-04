3b11a9c7dd0f3616822265f12bd63c30ae469516
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-12-04 12:40:22 +0100

    migration test working

---
+++ bonita-migration-integration-tests/src/test/java/org/bonitasoft/migration/MigrationIT.java
@@ -0,0 +1,22 @@
+package org.bonitasoft.migration;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
+
+@RunWith(AllTests.class)
+public class MigrationIT {
+
+    public static TestSuite suite() throws Exception
+    {
+        TestSuite suite = new TestSuite();
+        String nameOfClassToRun = "org.bonitasoft.migration.DatabaseChecker" + System.getProperty("target.version").replace('.', '_');
+        System.out.println("test class:" + nameOfClassToRun);
+        Class<?> testClass = Class.forName(nameOfClassToRun);
+        System.out.println(testClass);
+        suite.addTest(new JUnit4TestAdapter(testClass));
+        return suite;
+    }
+}
