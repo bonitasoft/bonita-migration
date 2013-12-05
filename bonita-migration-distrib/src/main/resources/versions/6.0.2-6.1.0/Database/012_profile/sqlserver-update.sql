ceea368369ffb13fa48dbae0413d456eb25e854b
Céline Souchet <celine.souchet@bonitasoft.com>
2013-12-05 09:47:11 +0100

    SQLServer : For Profile, remove a bad character ","

--- bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Database/012_profile/sqlserver-update.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Database/012_profile/sqlserver-update.sql
@@ -2,7 +2,7 @@ INSERT INTO profileentry (tenantId, id, profileId, name, description, parentId, 
 VALUES  (
 	:tenantId, 
 	(SELECT MAX(id) + 1 FROM profileentry), 
-	:admin_profile_id, , 
+	:admin_profile_id,
 	'Import / Export', 'Import / Export an final organization', 
 	:dir_profile_entry_id, 
 	6, 'link', 'importexportorganization')
