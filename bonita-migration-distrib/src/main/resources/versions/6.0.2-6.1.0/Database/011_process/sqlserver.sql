cc318646cd08b24b411fed813d954a4a3e7a0f51
Céline Souchet <celine.souchet@bonitasoft.com>
2013-12-05 09:06:13 +0100

    Revert bad change on process instance for SQLServer

--- bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Database/011_process/sqlserver.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Database/011_process/sqlserver.sql
@@ -12,7 +12,9 @@ UPDATE arch_process_instance SET startedByDelegate = startedBy
 -- process_instance
 -- 
 
-ALTER TABLE process_instance ALTER COLUMN startedBy NUMERIC(19,0) NOT NULL DEFAULT 0
+UPDATE process_instance SET startedBy=0 WHERE startedBy IS NULL
+@@
+ALTER TABLE process_instance ALTER COLUMN startedBy NUMERIC(19,0) NOT NULL
 @@
 ALTER TABLE process_instance ADD startedByDelegate NUMERIC(19,0) NOT NULL DEFAULT 0
 @@
