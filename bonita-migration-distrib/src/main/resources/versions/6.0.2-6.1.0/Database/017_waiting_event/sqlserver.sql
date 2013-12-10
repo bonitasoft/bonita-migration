d91bc939ce71387ff5e8493f0f5bbb3b4e03c988
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-12-10 16:55:14 +0100

    fix progess = null on sql server

--- bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Database/017_waiting_event/sqlserver.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.0.2-6.1.0/Database/017_waiting_event/sqlserver.sql
@@ -1,5 +1,7 @@
 ALTER TABLE waiting_event ADD progress TINYINT DEFAULT 0
 @@
+UPDATE waiting_event SET progress = 0
+@@
 UPDATE waiting_event SET parentProcessInstanceId = rootProcessInstanceId WHERE parentProcessInstanceId = -1
 @@
 DELETE FROM message_instance WHERE handled = 1
