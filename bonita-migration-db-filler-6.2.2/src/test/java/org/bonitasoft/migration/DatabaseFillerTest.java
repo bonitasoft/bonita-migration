493dfd20de09b0289ce6c7d9e8c8a64ecc7245b1
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2014-01-31 15:20:46 +0100

    fix db filler tests

--- bonita-migration-db-filler-6.2.2/src/test/java/org/bonitasoft/migration/DatabaseFillerTest.java
+++ bonita-migration-db-filler-6.2.2/src/test/java/org/bonitasoft/migration/DatabaseFillerTest.java
@@ -1,13 +1,12 @@
 package org.bonitasoft.migration;
 
-import org.bonitasoft.migration.DatabaseFiller6_2_0;
 import org.junit.Test;
 
 public class DatabaseFillerTest {
 
     @Test
     public void testMain() throws Exception {
-        DatabaseFiller6_2_0.main(null);
+        DatabaseFiller6_2_2.main(null);
     }
 
 }
