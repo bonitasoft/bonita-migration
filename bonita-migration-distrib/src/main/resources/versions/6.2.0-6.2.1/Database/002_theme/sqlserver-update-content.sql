5812c98f1d6bc62e848818bc7eaacc53129ca5e8
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2014-02-05 12:17:43 +0100

    fix sql server blob convertion

--- bonita-migration-distrib/src/main/resources/versions/6.2.0-6.2.1/Database/002_theme/sqlserver-update-content.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.2.0-6.2.1/Database/002_theme/sqlserver-update-content.sql
@@ -1,1 +1,1 @@
-UPDATE theme SET content = ?, cssContent = ? WHERE isDefault = 1 AND type = ?
+UPDATE theme SET content = CONVERT(VARBINARY(MAX), ?), cssContent = CONVERT(VARBINARY(MAX), ?) WHERE isDefault = 1 AND type = ?
