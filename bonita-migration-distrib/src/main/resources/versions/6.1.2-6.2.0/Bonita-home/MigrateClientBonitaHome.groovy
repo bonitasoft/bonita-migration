f996ce5893254d3d56e54236dac2aaa3f5ea26c1
Céline Souchet <celine.souchet@bonitasoft.com>
2014-02-17 16:41:09 +0100

    BS-7284 : Migration 6.1.2 -> 6.2.0 : FileNotFoundException: bonita\client\tenants\1\work\theme\mobile\.lastupdate
    - Remove migration for mobile theme

--- bonita-migration-distrib/src/main/resources/versions/6.1.2-6.2.0/Bonita-home/MigrateClientBonitaHome.groovy
+++ bonita-migration-distrib/src/main/resources/versions/6.1.2-6.2.0/Bonita-home/MigrateClientBonitaHome.groovy
@@ -1,10 +1,11 @@
 import org.bonitasoft.migration.core.MigrationUtil;
 import org.bonitasoft.migration.core.IOUtil;
 
+
 def currentDir = ""
 
-def File newClientBonitaHome = new File(newBonitaHome, "/client/");
-def File oldClientBonitaHome = new File(bonitaHome, "/client/");
+def File newClientBonitaHome = new File(newBonitaHome, "/client/")
+def File oldClientBonitaHome = new File(bonitaHome, "/client/")
 
 currentDir = "/platform/conf"
 MigrationUtil.migrateDirectory(newClientBonitaHome.path + currentDir, oldClientBonitaHome.path + currentDir, true)
@@ -18,11 +19,11 @@ MigrationUtil.migrateDirectory(newClientBonitaHome.path + currentDir, oldClientB
 println "Checking for tenants."
 def tenantsClientDir = new File(oldClientBonitaHome, "/tenants")
 if (tenantsClientDir.exists()) {
-    def tenants = Arrays.asList(tenantsClientDir.listFiles());
+    def tenants = Arrays.asList(tenantsClientDir.listFiles())
     if (tenants.empty){
         println "No tenants found."
     } else {
-        println "Executing update for each tenant : " + tenants;
+        println "Executing update for each tenant : " + tenants
         tenantsClientDir.eachDir { tenant ->
             println "For tenant : " + tenant.name
             PrintStream stdout = IOUtil.executeWrappedWithTabs {
@@ -41,15 +42,11 @@ if (tenantsClientDir.exists()) {
                 //first version that have theme so we create the folder
                 currentDir = "/work/theme"
                 MigrationUtil.migrateDirectory(newClientBonitaHome.path + "/platform/tenant-template" + currentDir, tenant.path + currentDir, false)
-                
-                portalDir = tenant.path + currentDir + "/portal"
-                mobileDir = tenant.path + currentDir + "/mobile"
                 //make the portal believe the theme is outdated so it's reloaded
-                new File(portalDir+"/.lastupdate").withWriter {it << "0"}
-                new File(mobileDir+"/.lastupdate").withWriter {it << "0"}
-                currentDir = "/work/looknfeel"
-                def fileDir = new File(currentDir);
-                IOUtil.deleteDirectory(fileDir);
+                new File(tenant.path + currentDir + "/portal/.lastupdate").withWriter {it << "0"}
+
+                def fileDir = new File("/work/looknfeel")
+                IOUtil.deleteDirectory(fileDir)
             }
         }
     }
