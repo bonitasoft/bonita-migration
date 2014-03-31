a62fc2e2f94c8f6d39809cc134458e4d558c1f88
Céline Souchet <celine.souchet@bonitasoft.com>
2014-03-31 14:27:01 +0200

    BS-7949 : Add migration from 6.2.5 -> 6.3.0 in the migration tool
    - Fix bad SQL scripts

--- bonita-migration-distrib/src/main/resources/versions/6.2.5-6.3.0/Database/003_profile_process_manager/sqlserver-insert_profileentry_for_process_manager.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.2.5-6.3.0/Database/003_profile_process_manager/sqlserver-insert_profileentry_for_process_manager.sql
@@ -1,26 +1,26 @@
-INSERT INTO profileentry (tenantId, id, profileId, name, description, index_, type, page)
+INSERT INTO profileentry (tenantId, id, profileId, parentId, name, description, index_, type, page)
 VALUES  (
 	:tenantId,
 	(SELECT MAX(id) + 1 FROM profileentry),
-	:process_manager_profile_id, 	
+	:process_manager_profile_id, 0,
 	'Tasks', 'My tasks', 
 	0, 'link', 'tasklistingpm')
 @@
 	
-INSERT INTO profileentry (tenantId, id, profileId, name, description, index_, type, page)
+INSERT INTO profileentry (tenantId, id, profileId, parentId, name, description, index_, type, page)
 VALUES  (
 	:tenantId,
 	(SELECT MAX(id) + 1 FROM profileentry),
-	:process_manager_profile_id, 	
+	:process_manager_profile_id, 0,
 	'Cases', 'My cases', 
 	2, 'link', 'caselistingpm')
 @@
 	
-INSERT INTO profileentry (tenantId, id, profileId, name, description, index_, type, page)
+INSERT INTO profileentry (tenantId, id, profileId, parentId, name, description, index_, type, page)
 VALUES  (
 	:tenantId,
 	(SELECT MAX(id) + 1 FROM profileentry),
-	:process_manager_profile_id, 	
+	:process_manager_profile_id, 0,
 	'Apps', 'My processes', 
 	4, 'link', 'processlistingpm')
 @@
