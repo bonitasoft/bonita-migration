6504a18b0602efdeab860bf441b87427a0f59e35
Céline Souchet <celine.souchet@bonitasoft.com>
2014-03-31 12:06:06 +0200

    BS-7949 : Add migration from 6.2.5 -> 6.3.0 in the migration tool
    - Fix bad name of SQL script

--- bonita-migration-distrib/src/main/resources/versions/6.2.5-6.3.0/Database/003_profile_process_manager/MigrateFeature.groovy
+++ bonita-migration-distrib/src/main/resources/versions/6.2.5-6.3.0/Database/003_profile_process_manager/MigrateFeature.groovy
@@ -1,4 +1,3 @@
-import org.bonitasoft.migration.core.MigrationUtil;
 
 def currentTime = System.currentTimeMillis()
 def tenants = MigrationUtil.getTenantsId(feature, dbVendor, sql)
@@ -6,18 +5,18 @@ def tenants = MigrationUtil.getTenantsId(feature, dbVendor, sql)
 println "Executing update for each tenant : " + tenants
 tenants.each {
     println "For tenant with id = " + it
-    
+
     // Create the new profile in first
-    def parameters = new HashMap();
+    def parameters = new HashMap()
     parameters.put(":tenantId", String.valueOf(it))
     parameters.put(":creationDate", new Date().getTime())
-    MigrationUtil.executeSqlFile(feature, dbVendor, "insert_process-manager-profile", parameters, sql, false)
-    
-    
-    // There is a needed profile 
+    MigrationUtil.executeSqlFile(feature, dbVendor, "insert_process_manager_profile", parameters, sql, false)
+
+
+    // There is a needed profile
     def processManagerId = MigrationUtil.getId(feature, dbVendor, "get_process_manager_profile_id", it, sql)
 
-    
+
     // Then, create its profile entries
     if(processManagerId != null){
         parameters = new HashMap()
