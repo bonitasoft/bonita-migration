b60830cd3a31cc694e161ffb8fbb9bff1339cddf
Céline Souchet <celine.souchet@bonitasoft.com>
2014-03-31 13:46:40 +0200

    BS-7949 : Add migration from 6.2.5 -> 6.3.0 in the migration tool
    - Fix bad removed import

--- bonita-migration-distrib/src/main/resources/versions/6.2.5-6.3.0/Database/003_profile_process_manager/MigrateFeature.groovy
+++ bonita-migration-distrib/src/main/resources/versions/6.2.5-6.3.0/Database/003_profile_process_manager/MigrateFeature.groovy
@@ -1,3 +1,4 @@
+import org.bonitasoft.migration.core.MigrationUtil;
 
 def currentTime = System.currentTimeMillis()
 def tenants = MigrationUtil.getTenantsId(feature, dbVendor, sql)
@@ -5,18 +6,18 @@ def tenants = MigrationUtil.getTenantsId(feature, dbVendor, sql)
 println "Executing update for each tenant : " + tenants
 tenants.each {
     println "For tenant with id = " + it
-
+    
     // Create the new profile in first
-    def parameters = new HashMap()
+    def parameters = new HashMap();
     parameters.put(":tenantId", String.valueOf(it))
     parameters.put(":creationDate", new Date().getTime())
     MigrationUtil.executeSqlFile(feature, dbVendor, "insert_process_manager_profile", parameters, sql, false)
-
-
-    // There is a needed profile
+    
+    
+    // There is a needed profile 
     def processManagerId = MigrationUtil.getId(feature, dbVendor, "get_process_manager_profile_id", it, sql)
 
-
+    
     // Then, create its profile entries
     if(processManagerId != null){
         parameters = new HashMap()
