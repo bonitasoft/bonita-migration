30419f057da81db92a914193e6985c2da11e9048
Céline Souchet <celine.souchet@bonitasoft.com>
2014-04-17 10:11:39 +0200

    BS-8394 : Rename startedByDelegate in startedBySubstitute & executedByDelegate in executedBySubstitute
    - Add migration for archived process instance & flow node instance

--- bonita-migration-distrib/src/main/resources/versions/6.2.6-6.3.0/Database/008_start_or_execute_for/mysql.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.2.6-6.3.0/Database/008_start_or_execute_for/mysql.sql
@@ -1,3 +1,5 @@
 ALTER TABLE process_instance CHANGE startedByDelegate startedBySubstitute;
+ALTER TABLE arch_process_instance CHANGE startedByDelegate startedBySubstitute;
 
 ALTER TABLE flownode_instance CHANGE executedByDelegate executedBySubstitute;
+ALTER TABLE arch_flownode_instance CHANGE executedByDelegate executedBySubstitute;
