1244d1e4f00ac5bec05d2fe2d159489a8fb533a2
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2014-07-30 15:41:08 +0200

    BS-8940, oracle syntax

--- bonita-migration-distrib/src/main/resources/versions/6.3.2-6.4.0/Database/002_business_data/oracle-update.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.3.2-6.4.0/Database/002_business_data/oracle-update.sql
@@ -1,10 +1,10 @@
-ALTER TABLE ref_biz_data_inst ADD kind VARCHAR2(10);
+ALTER TABLE ref_biz_data_inst ADD kind VARCHAR2(10)
 @@
 
-UPDATE ref_biz_data_inst SET kind="simple_ref";
+UPDATE ref_biz_data_inst SET kind="simple_ref"
 @@
 
-ALTER TABLE ref_biz_data_inst MODIFY kind NOT NULL;
+ALTER TABLE ref_biz_data_inst MODIFY kind NOT NULL
 @@
 
 CREATE TABLE multi_biz_data (
@@ -13,8 +13,8 @@ CREATE TABLE multi_biz_data (
   	idx NUMBER(19, 0) NOT NULL,
   	data_id NUMBER(19, 0) NOT NULL,
   	PRIMARY KEY (tenantid, id, data_id)
-);
+)
 @@
 
-ALTER TABLE multi_biz_data ADD CONSTRAINT fk_rbdi_mbd FOREIGN KEY (tenantid, id) REFERENCES ref_biz_data_inst(tenantid, id) ON DELETE CASCADE;
+ALTER TABLE multi_biz_data ADD CONSTRAINT fk_rbdi_mbd FOREIGN KEY (tenantid, id) REFERENCES ref_biz_data_inst(tenantid, id) ON DELETE CASCADE
 @@
