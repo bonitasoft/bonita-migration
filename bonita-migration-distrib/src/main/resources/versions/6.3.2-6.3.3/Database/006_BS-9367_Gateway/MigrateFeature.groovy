a65ec7a2b967a046c0d486ae9fbb1e141fa1c3ac
Céline Souchet <celine.souchet@bonitasoft.com>
2014-08-01 14:42:50 +0200

    BS-9367 : CLONE - [6.1.2] And Join Gateways going into failed state
    - Change logs

--- bonita-migration-distrib/src/main/resources/versions/6.3.2-6.3.3/Database/006_BS-9367_Gateway/MigrateFeature.groovy
+++ bonita-migration-distrib/src/main/resources/versions/6.3.2-6.3.3/Database/006_BS-9367_Gateway/MigrateFeature.groovy
@@ -25,10 +25,10 @@ IOUtil.executeWrappedWithTabs {
                 def List<Long> archGatesInUnknownState = MigrationUtil.getIds(feature, dbVendor, "get_archived_gateway_instances_in_unknown_state_id", parameters, sql)
              
                 if (archGatesInUnknownState.isEmpty()) {  
-                   println "Updating the state of the gateway with the id " + failedGatewayId
+                   println "Updating the state of the gateway with the id = " + failedGatewayId
                    MigrationUtil.executeSqlFile(feature, dbVendor, "update_gateway_instance_state", parameters, sql, false)
                     
-                   println "Deleting the archived gateway related to the gateway with the id " + failedGatewayId
+                   println "Deleting the archived gateway related to the gateway with the id = " + failedGatewayId
                    MigrationUtil.executeSqlFile(feature, dbVendor, "delete_archived_gateway_instances", parameters, sql, false)
                 } else {
                    println "**** Unknown gateway entry found for " + failedGatewayId + ", no database changes made"
