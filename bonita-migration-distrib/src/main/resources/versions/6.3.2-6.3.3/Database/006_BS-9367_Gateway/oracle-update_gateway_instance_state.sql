e0e0e7fbf501746eb4211c5e3f885f348ace5be5
Céline Souchet <celine.souchet@bonitasoft.com>
2014-08-01 11:10:44 +0200

    BS-9367 : CLONE - [6.1.2] And Join Gateways going into failed state
    - Fix Oracle SQL scripts

--- bonita-migration-distrib/src/main/resources/versions/6.3.2-6.3.3/Database/006_BS-9367_Gateway/oracle-update_gateway_instance_state.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.3.2-6.3.3/Database/006_BS-9367_Gateway/oracle-update_gateway_instance_state.sql
@@ -1,14 +1,12 @@
-UPDATE (
-    SELECT flownode_instance.*, arch_flownode_instance.*
-    FROM flownode_instance
-        LEFT JOIN arch_flownode_instance
-        ON flownode_instance.id = arch_flownode_instance.sourceObjectId
-	WHERE arch_flownode_instance.sourceObjectId = :flowNodeInstanceId
+MERGE INTO flownode_instance 
+	USING arch_flownode_instance 
+	ON flownode_instance.id = arch_flownode_instance.sourceObjectId
+	AND arch_flownode_instance.sourceObjectId = :flowNodeInstanceId
 	AND arch_flownode_instance.stateName = 'executing'
 	AND flownode_instance.tenantId = :tenantId
 	AND flownode_instance.kind = 'gate'
-)
-SET
+WHEN MATCHED THEN UPDATE 
+SET 
     flownode_instance.terminal = arch_flownode_instance.terminal,
     flownode_instance.stable = arch_flownode_instance.stable,
     flownode_instance.stateId = arch_flownode_instance.stateId,
