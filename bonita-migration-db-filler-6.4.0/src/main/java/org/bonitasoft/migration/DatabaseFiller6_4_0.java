92f9d82ea66c598fd40c77b55d06412e20f9d68e
Céline Souchet <celine.souchet@bonitasoft.com>
2014-08-07 10:27:25 +0200

    - Rename migration 6.3.2-6.4.0 in 6.3.3-6.4.0 - Add integration tests for 6.3.3 - Add bonita homes & themes for 6.3.3

--- bonita-migration-db-filler-6.4.0/src/main/java/org/bonitasoft/migration/DatabaseFiller6_4_0.java
+++ bonita-migration-db-filler-6.4.0/src/main/java/org/bonitasoft/migration/DatabaseFiller6_4_0.java
@@ -13,14 +13,11 @@
  **/
 package org.bonitasoft.migration;
 
-import org.bonitasoft.engine.exception.BonitaException;
-import org.bonitasoft.engine.test.PlatformTestUtil;
-
-public class DatabaseFiller6_4_0 extends SimpleDatabaseFiller6_3_1 {
+public class DatabaseFiller6_4_0 extends DatabaseFiller6_3_3 {
 
     public static void main(final String[] args) throws Exception {
         DatabaseFiller6_4_0 databaseFiller = new DatabaseFiller6_4_0();
         databaseFiller.execute(1, 1, 1, 1);
     }
-    
+
 }
