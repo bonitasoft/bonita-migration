671d22b8b15ce51df45be530758af4ebf1082648
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2014-09-05 17:39:39 +0200

    BS-9678, add script for dbunit tests for all databases

--- bonita-migration-scripts-integration-tests/src/main/resources/sql/v6_4_0/mysql-documents.sql
+++ bonita-migration-scripts-integration-tests/src/main/resources/sql/v6_4_0/mysql-documents.sql
@@ -1,9 +1,48 @@
-CREATE TABLE multi_biz_data (
-	tenantid BIGINT NOT NULL,
-  	id BIGINT NOT NULL,
-  	idx BIGINT NOT NULL,
-  	data_id BIGINT NOT NULL,
-  	PRIMARY KEY (tenantid, id, data_id)
+CREATE TABLE tenant (
+  id BIGINT NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE = INNODB;
-
-ALTER TABLE multi_biz_data ADD CONSTRAINT fk_rbdi_mbd FOREIGN KEY (tenantid, id) REFERENCES ref_biz_data_inst(tenantid, id) ON DELETE CASCADE;
+CREATE TABLE sequence (
+  tenantid BIGINT NOT NULL,
+  id BIGINT NOT NULL,
+  nextid BIGINT NOT NULL,
+  PRIMARY KEY (tenantid, id)
+) ENGINE = INNODB;
+CREATE TABLE arch_document_mapping (
+  tenantid BIGINT NOT NULL,
+  id BIGINT NOT NULL,
+  processinstanceid BIGINT,
+  sourceObjectId BIGINT,
+  documentName VARCHAR(50) NOT NULL,
+  documentAuthor BIGINT,
+  documentCreationDate BIGINT NOT NULL,
+  documentHasContent BOOLEAN NOT NULL,
+  documentContentFileName VARCHAR(255),
+  documentContentMimeType VARCHAR(255),
+  contentStorageId VARCHAR(50),
+  documentURL VARCHAR(255),
+  archiveDate BIGINT NOT NULL,
+  PRIMARY KEY (tenantid, ID)
+) ENGINE = INNODB;
+CREATE TABLE document_mapping (
+  tenantid BIGINT NOT NULL,
+  id BIGINT NOT NULL,
+  processinstanceid BIGINT,
+  documentName VARCHAR(50) NOT NULL,
+  documentAuthor BIGINT,
+  documentCreationDate BIGINT NOT NULL,
+  documentHasContent BOOLEAN NOT NULL,
+  documentContentFileName VARCHAR(255),
+  documentContentMimeType VARCHAR(255),
+  contentStorageId VARCHAR(50),
+  documentURL VARCHAR(255),
+  PRIMARY KEY (tenantid, ID)
+) ENGINE = INNODB;
+CREATE TABLE document_content (
+  tenantid BIGINT NOT NULL,
+  id BIGINT NOT NULL,
+  documentId VARCHAR(50) NOT NULL,
+  content LONGBLOB NOT NULL,
+  PRIMARY KEY (tenantid, id)
+) ENGINE = INNODB;
+ALTER TABLE document_content ADD CONSTRAINT fk_document_content_tenantId FOREIGN KEY (tenantid) REFERENCES tenant(id);
