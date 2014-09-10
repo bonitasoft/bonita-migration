90c2c9b9d6aa8f6c49f7555417541ab5f802865d
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2014-09-10 17:17:52 +0200

    BS-8991, allow 255 characters for username, firstName, lastName, jobTitle and contactInfo.address

--- bonita-migration-db-filler-6.3.6/src/main/java/org/bonitasoft/migration/DatabaseChecker6_3_6.java
+++ bonita-migration-db-filler-6.3.6/src/main/java/org/bonitasoft/migration/DatabaseChecker6_3_6.java
@@ -13,19 +13,62 @@
  **/
 package org.bonitasoft.migration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.bonitasoft.engine.identity.ContactDataCreator;
+import org.bonitasoft.engine.identity.User;
+import org.bonitasoft.engine.identity.UserCreator;
+import org.bonitasoft.engine.identity.UserWithContactData;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 
 
 public class DatabaseChecker6_3_6 extends SimpleDatabaseChecker6_3_2 {
-    
+
     public static void main(final String[] args) throws Exception {
         JUnitCore.main(DatabaseChecker6_3_6.class.getName());
     }
 
+    //BS-8991
     @Test
-    public void callApi(){
-        processAPI.getNumberOfProcessDeploymentInfos();
+    public void can_create_user_with_255_char_in_fields() throws Exception {
+        //given
+        final UserCreator creator = new UserCreator(completeWithZeros("user"), "bpm");
+        creator.setJobTitle(completeWithZeros("Engineer"));
+        creator.setFirstName(completeWithZeros("First"));
+        creator.setLastName(completeWithZeros("Last"));
+
+        final ContactDataCreator contactDataCreator = new ContactDataCreator();
+        contactDataCreator.setAddress(completeWithZeros("32 Rue Gustave Eiffel"));
+        creator.setProfessionalContactData(contactDataCreator);
+
+        //when
+        final User user = identityApi.createUser(creator);
+
+        //then
+        assertThat(user).isNotNull();
+        assertThat(user.getUserName()).hasSize(255);
+        assertThat(user.getFirstName()).hasSize(255);
+        assertThat(user.getLastName()).hasSize(255);
+        assertThat(user.getJobTitle()).hasSize(255);
+
+        //when
+        final UserWithContactData userWithContactData = identityApi.getUserWithProfessionalDetails(user.getId());
+
+        //then
+        assertThat(userWithContactData).isNotNull();
+        assertThat(userWithContactData.getContactData().getAddress()).hasSize(255);
+
+    }
+
+    private String completeWithZeros(final String prefix) {
+
+        final StringBuilder stb = new StringBuilder(prefix);
+        for (int i = 0; i < 255 - prefix.length(); i++) {
+            stb.append("0");
+        }
+
+        return stb.toString();
     }
 
 }
