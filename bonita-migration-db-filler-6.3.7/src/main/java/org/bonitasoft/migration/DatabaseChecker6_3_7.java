49171fa0641508800680d24dce936b3db70cf2d5
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2014-09-16 14:50:10 +0200

    move migration of features from 6.3.6 to 6.3.7

--- bonita-migration-db-filler-6.3.7/src/main/java/org/bonitasoft/migration/DatabaseChecker6_3_7.java
+++ bonita-migration-db-filler-6.3.7/src/main/java/org/bonitasoft/migration/DatabaseChecker6_3_7.java
@@ -13,6 +13,15 @@
  **/
 package org.bonitasoft.migration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.bonitasoft.engine.bpm.document.Document;
+import org.bonitasoft.engine.bpm.process.ProcessInstance;
+import org.bonitasoft.engine.identity.ContactDataCreator;
+import org.bonitasoft.engine.identity.User;
+import org.bonitasoft.engine.identity.UserCreator;
+import org.bonitasoft.engine.identity.UserWithContactData;
+import org.junit.Test;
 import org.junit.runner.JUnitCore;
 
 
@@ -20,6 +29,68 @@ public class DatabaseChecker6_3_7 extends SimpleDatabaseChecker6_3_2 {
     
     public static void main(final String[] args) throws Exception {
         JUnitCore.main(DatabaseChecker6_3_7.class.getName());
+    }
+
+
+    //BS-8991
+    @Test
+    public void can_create_user_with_255_char_in_fields() throws Exception {
+        //given
+        final UserCreator creator = new UserCreator(completeWithZeros("user"), "bpm");
+        creator.setJobTitle(completeWithZeros("Engineer"));
+        creator.setFirstName(completeWithZeros("First"));
+        creator.setLastName(completeWithZeros("Last"));
+
+        final ContactDataCreator contactDataCreator = new ContactDataCreator();
+        contactDataCreator.setAddress(completeWithZeros("32 Rue Gustave Eiffel"));
+        creator.setProfessionalContactData(contactDataCreator);
+
+        //when
+        final User user = identityApi.createUser(creator);
+
+        //then
+        assertThat(user).isNotNull();
+        assertThat(user.getUserName()).hasSize(255);
+        assertThat(user.getFirstName()).hasSize(255);
+        assertThat(user.getLastName()).hasSize(255);
+        assertThat(user.getJobTitle()).hasSize(255);
+
+        //when
+        final UserWithContactData userWithContactData = identityApi.getUserWithProfessionalDetails(user.getId());
+
+        //then
+        assertThat(userWithContactData).isNotNull();
+        assertThat(userWithContactData.getContactData().getAddress()).hasSize(255);
+
+    }
+
+
+    private String completeWithZeros(final String prefix) {
+
+        final StringBuilder stb = new StringBuilder(prefix);
+        for (int i = 0; i < 255 - prefix.length(); i++) {
+            stb.append("0");
+        }
+
+        return stb.toString();
+    }
+
+    @Test
+    public void should_allow_documents_with_null_content() throws Exception {
+        ProcessInstance instance = processAPI.startProcess(processAPI.getProcessDefinitionId(
+                SimpleDatabaseFiller6_0_2.PROCESS_NAME,
+                SimpleDatabaseFiller6_0_2.PROCESS_VERSION));
+
+        Document document = processAPI.attachDocument(
+                instance.getId(),
+                "Document Name",
+                "File Name",
+                "Mime",
+                (byte[]) null);
+
+        assertThat(processAPI.getDocumentContent(document.getContentStorageId())).isEqualTo(new byte[0]);
+
+        processAPI.deleteProcessInstance(instance.getId());
     }
 
 }
