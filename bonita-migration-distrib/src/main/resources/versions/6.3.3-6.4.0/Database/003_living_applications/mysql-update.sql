ba851a4816fd58d3e0255096d7dc6c1a55424a07
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2014-10-29 09:58:07 +0100

    Revert "BS-11151, remove on delete cascate (not supported by SQL Server). Delete will be managed by Java code"
    This reverts commit 20f672ba57e6c513a26620f42e35601cc15a3a1a.

--- bonita-migration-distrib/src/main/resources/versions/6.3.3-6.4.0/Database/003_living_applications/mysql-update.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.3.3-6.4.0/Database/003_living_applications/mysql-update.sql
@@ -70,7 +70,7 @@ ALTER TABLE business_app_menu ADD CONSTRAINT fk_app_menu_tenantId FOREIGN KEY (t
 @@
 ALTER TABLE business_app_menu ADD CONSTRAINT fk_app_menu_appId FOREIGN KEY (tenantid, applicationId) REFERENCES business_app (tenantid, id) ON DELETE CASCADE;
 @@
-ALTER TABLE business_app_menu ADD CONSTRAINT fk_app_menu_pageId FOREIGN KEY (tenantid, applicationPageId) REFERENCES business_app_page (tenantid, id);
+ALTER TABLE business_app_menu ADD CONSTRAINT fk_app_menu_pageId FOREIGN KEY (tenantid, applicationPageId) REFERENCES business_app_page (tenantid, id) ON DELETE CASCADE;
 @@
 ALTER TABLE business_app_menu ADD CONSTRAINT fk_app_menu_parentId FOREIGN KEY (tenantid, parentId) REFERENCES business_app_menu (tenantid, id);
 @@
