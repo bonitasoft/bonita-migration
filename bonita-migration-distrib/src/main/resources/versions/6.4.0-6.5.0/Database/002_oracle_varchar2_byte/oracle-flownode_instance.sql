eb984aaf60d0d161a97cddb5dea4b3ef11201e94
Céline Souchet <celine.souchet@bonitasoft.com>
2014-12-17 17:44:54 +0100

    BS-6831 : [encoding] ORA-12899: Fields with non-ascii characters value too large for column in Oracle db
    - Fix bad name for temporary columns

--- bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/002_oracle_varchar2_byte/oracle-flownode_instance.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/002_oracle_varchar2_byte/oracle-flownode_instance.sql
@@ -28,19 +28,19 @@ ALTER TABLE flownode_instance DROP COLUMN displayDescription @@
 ALTER TABLE flownode_instance RENAME COLUMN displayDescription_temp TO displayDescription @@
 
 
-ALTER TABLE flownode_instance ADD stateName VARCHAR2(50 CHAR) @@
+ALTER TABLE flownode_instance ADD stateName_temp VARCHAR2(50 CHAR) @@
 UPDATE flownode_instance SET stateName_temp = stateName @@
 ALTER TABLE flownode_instance DROP COLUMN stateName @@
 ALTER TABLE flownode_instance RENAME COLUMN stateName_temp TO stateName @@
 
 
-ALTER TABLE flownode_instance ADD gatewayType VARCHAR2(50 CHAR) @@
+ALTER TABLE flownode_instance ADD gatewayType_temp VARCHAR2(50 CHAR) @@
 UPDATE flownode_instance SET gatewayType_temp = gatewayType @@
 ALTER TABLE flownode_instance DROP COLUMN gatewayType @@
 ALTER TABLE flownode_instance RENAME COLUMN gatewayType_temp TO gatewayType @@
 
 
-ALTER TABLE flownode_instance ADD hitBys VARCHAR2(255 CHAR) @@
+ALTER TABLE flownode_instance ADD hitBys_temp VARCHAR2(255 CHAR) @@
 UPDATE flownode_instance SET hitBys_temp = hitBys @@
 ALTER TABLE flownode_instance DROP COLUMN hitBys @@
 ALTER TABLE flownode_instance RENAME COLUMN hitBys_temp TO hitBys @@
@@ -53,31 +53,31 @@ ALTER TABLE flownode_instance RENAME COLUMN stateCategory_temp TO stateCategory 
 ALTER TABLE flownode_instance MODIFY stateCategory NOT NULL @@
 
 
-ALTER TABLE flownode_instance ADD description VARCHAR2(255 CHAR) @@
+ALTER TABLE flownode_instance ADD description_temp VARCHAR2(255 CHAR) @@
 UPDATE flownode_instance SET description_temp = description @@
 ALTER TABLE flownode_instance DROP COLUMN description @@
 ALTER TABLE flownode_instance RENAME COLUMN description_temp TO description @@
 
 
-ALTER TABLE flownode_instance ADD loopDataInputRef VARCHAR2(255 CHAR) @@
+ALTER TABLE flownode_instance ADD loopDataInputRef_temp VARCHAR2(255 CHAR) @@
 UPDATE flownode_instance SET loopDataInputRef_temp = loopDataInputRef @@
 ALTER TABLE flownode_instance DROP COLUMN loopDataInputRef @@
 ALTER TABLE flownode_instance RENAME COLUMN loopDataInputRef_temp TO loopDataInputRef @@
 
 
-ALTER TABLE flownode_instance ADD loopDataOutputRef VARCHAR2(255 CHAR) @@
+ALTER TABLE flownode_instance ADD loopDataOutputRef_temp VARCHAR2(255 CHAR) @@
 UPDATE flownode_instance SET loopDataOutputRef_temp = loopDataOutputRef @@
 ALTER TABLE flownode_instance DROP COLUMN loopDataOutputRef @@
 ALTER TABLE flownode_instance RENAME COLUMN loopDataOutputRef_temp TO loopDataOutputRef @@
 
 
-ALTER TABLE flownode_instance ADD dataInputItemRef VARCHAR2(255 CHAR) @@
+ALTER TABLE flownode_instance ADD dataInputItemRef_temp VARCHAR2(255 CHAR) @@
 UPDATE flownode_instance SET dataInputItemRef_temp = dataInputItemRef @@
 ALTER TABLE flownode_instance DROP COLUMN dataInputItemRef @@
 ALTER TABLE flownode_instance RENAME COLUMN dataInputItemRef_temp TO dataInputItemRef @@
 
 
-ALTER TABLE flownode_instance ADD dataOutputItemRef VARCHAR2(255 CHAR) @@
+ALTER TABLE flownode_instance ADD dataOutputItemRef_temp VARCHAR2(255 CHAR) @@
 UPDATE flownode_instance SET dataOutputItemRef_temp = dataOutputItemRef @@
 ALTER TABLE flownode_instance DROP COLUMN dataOutputItemRef @@
 ALTER TABLE flownode_instance RENAME COLUMN dataOutputItemRef_temp TO dataOutputItemRef @@
@@ -115,49 +115,49 @@ ALTER TABLE arch_flownode_instance DROP COLUMN displayDescription @@
 ALTER TABLE arch_flownode_instance RENAME COLUMN displayDescription_temp TO displayDescription @@
 
 
-ALTER TABLE arch_flownode_instance ADD stateName VARCHAR2(50 CHAR) @@
+ALTER TABLE arch_flownode_instance ADD stateName_temp VARCHAR2(50 CHAR) @@
 UPDATE arch_flownode_instance SET stateName_temp = stateName @@
 ALTER TABLE arch_flownode_instance DROP COLUMN stateName @@
 ALTER TABLE arch_flownode_instance RENAME COLUMN stateName_temp TO stateName @@
 
 
-ALTER TABLE arch_flownode_instance ADD gatewayType VARCHAR2(50 CHAR) @@
+ALTER TABLE arch_flownode_instance ADD gatewayType_temp VARCHAR2(50 CHAR) @@
 UPDATE arch_flownode_instance SET gatewayType_temp = gatewayType @@
 ALTER TABLE arch_flownode_instance DROP COLUMN gatewayType @@
 ALTER TABLE arch_flownode_instance RENAME COLUMN gatewayType_temp TO gatewayType @@
 
 
-ALTER TABLE arch_flownode_instance ADD hitBys VARCHAR2(255 CHAR) @@
+ALTER TABLE arch_flownode_instance ADD hitBys_temp VARCHAR2(255 CHAR) @@
 UPDATE arch_flownode_instance SET hitBys_temp = hitBys @@
 ALTER TABLE arch_flownode_instance DROP COLUMN hitBys @@
 ALTER TABLE arch_flownode_instance RENAME COLUMN hitBys_temp TO hitBys @@
 
 
-ALTER TABLE arch_flownode_instance ADD description VARCHAR2(255 CHAR) @@
+ALTER TABLE arch_flownode_instance ADD description_temp VARCHAR2(255 CHAR) @@
 UPDATE arch_flownode_instance SET description_temp = description @@
 ALTER TABLE arch_flownode_instance DROP COLUMN description @@
 ALTER TABLE arch_flownode_instance RENAME COLUMN description_temp TO description @@
 
 
-ALTER TABLE arch_flownode_instance ADD loopDataInputRef VARCHAR2(255 CHAR) @@
+ALTER TABLE arch_flownode_instance ADD loopDataInputRef_temp VARCHAR2(255 CHAR) @@
 UPDATE arch_flownode_instance SET loopDataInputRef_temp = loopDataInputRef @@
 ALTER TABLE arch_flownode_instance DROP COLUMN loopDataInputRef @@
 ALTER TABLE arch_flownode_instance RENAME COLUMN loopDataInputRef_temp TO loopDataInputRef @@
 
 
-ALTER TABLE arch_flownode_instance ADD loopDataOutputRef VARCHAR2(255 CHAR) @@
+ALTER TABLE arch_flownode_instance ADD loopDataOutputRef_temp VARCHAR2(255 CHAR) @@
 UPDATE arch_flownode_instance SET loopDataOutputRef_temp = loopDataOutputRef @@
 ALTER TABLE arch_flownode_instance DROP COLUMN loopDataOutputRef @@
 ALTER TABLE arch_flownode_instance RENAME COLUMN loopDataOutputRef_temp TO loopDataOutputRef @@
 
 
-ALTER TABLE arch_flownode_instance ADD dataInputItemRef VARCHAR2(255 CHAR) @@
+ALTER TABLE arch_flownode_instance ADD dataInputItemRef_temp VARCHAR2(255 CHAR) @@
 UPDATE arch_flownode_instance SET dataInputItemRef_temp = dataInputItemRef @@
 ALTER TABLE arch_flownode_instance DROP COLUMN dataInputItemRef @@
 ALTER TABLE arch_flownode_instance RENAME COLUMN dataInputItemRef_temp TO dataInputItemRef @@
 
 
-ALTER TABLE arch_flownode_instance ADD dataOutputItemRef VARCHAR2(255 CHAR) @@
+ALTER TABLE arch_flownode_instance ADD dataOutputItemRef_temp VARCHAR2(255 CHAR) @@
 UPDATE arch_flownode_instance SET dataOutputItemRef_temp = dataOutputItemRef @@
 ALTER TABLE arch_flownode_instance DROP COLUMN dataOutputItemRef @@
 ALTER TABLE arch_flownode_instance RENAME COLUMN dataOutputItemRef_temp TO dataOutputItemRef @@
