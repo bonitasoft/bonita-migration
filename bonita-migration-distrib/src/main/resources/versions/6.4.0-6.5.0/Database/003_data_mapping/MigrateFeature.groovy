f269f9fe3772f777672408ae885781a83bc4fd9b
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-01-05 17:51:33 +0100

    remove unused command

--- bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/003_data_mapping/MigrateFeature.groovy
+++ bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/003_data_mapping/MigrateFeature.groovy
@@ -1,3 +1,4 @@
 import org.bonitasoft.migration.core.MigrationUtil;
 
 MigrationUtil.executeDefaultSqlFile(feature, dbVendor, sql)
+
--- bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/003_data_mapping/oracle.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/003_data_mapping/oracle.sql
@@ -3,3 +3,6 @@ DROP TABLE "data_mapping" @@
 DROP TABLE "arch_data_mapping" @@
 DELETE FROM "SEQUENCE" WHERE "ID" = 10021 @@
 DELETE FROM "SEQUENCE" WHERE "ID" = 20051 @@
+DELETE FROM "COMMAND" WHERE "NAME" = getUpdatedVariableValuesForProcessDefinition@@
+DELETE FROM "COMMAND" WHERE "NAME" = getUpdatedVariableValuesForActivityInstance@@
+DELETE FROM "COMMAND" WHERE "NAME" = getUpdatedVariableValuesForProcessInstance@@
