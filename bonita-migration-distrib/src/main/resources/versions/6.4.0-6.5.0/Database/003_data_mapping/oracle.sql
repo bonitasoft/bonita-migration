880fffb6c8b3d11f52bb3488e96a6f4ef9ca862b
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-01-06 14:37:57 +0100

    update index of data instance tables

--- bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/003_data_mapping/oracle.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/003_data_mapping/oracle.sql
@@ -1,6 +1,10 @@
 DROP INDEX "idx_datamapp_container"@@
 DROP TABLE "data_mapping" @@
 DROP TABLE "arch_data_mapping" @@
+DROP INDEX "idx_datai_container"@@
+CREATE INDEX "idx_datai_container" ON data_instance (tenantId, containerId, containerType, name)@@
+DROP INDEX "idx1_arch_data_instance"@@
+CREATE INDEX "idx1_arch_data_instance" ON arch_data_instance (tenantId, containerId, containerType, archiveDate, name, sourceObjectId)@@
 DELETE FROM "SEQUENCE" WHERE "ID" = 10021 @@
 DELETE FROM "SEQUENCE" WHERE "ID" = 20051 @@
 DELETE FROM "COMMAND" WHERE "NAME" = 'getUpdatedVariableValuesForProcessDefinition'@@
--- bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/003_data_mapping/sqlserver.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.0-6.5.0/Database/003_data_mapping/sqlserver.sql
@@ -4,6 +4,14 @@ DROP TABLE data_mapping
 @@
 DROP TABLE arch_data_mapping
 @@
+DROP INDEX idx_datai_container ON data_instance
+@@
+CREATE INDEX idx_datai_container ON data_instance (tenantId, containerId, containerType, name)
+@@
+DROP INDEX idx1_arch_data_instance ON arch_data_instance
+@@
+CREATE INDEX idx1_arch_data_instance ON arch_data_instance (tenantId, containerId, containerType, archiveDate, name, sourceObjectId)
+@@
 DELETE FROM "SEQUENCE" WHERE "ID" = 10021
 @@
 DELETE FROM "SEQUENCE" WHERE "ID" = 20051
