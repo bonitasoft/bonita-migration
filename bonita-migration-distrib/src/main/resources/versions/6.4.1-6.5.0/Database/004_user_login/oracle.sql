ae11f123bcc282b203945a5c6770f7f82c917f5e
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-01-20 10:19:42 +0100

    BS-9967, fix migration on oracle and sqlserver

--- bonita-migration-distrib/src/main/resources/versions/6.4.1-6.5.0/Database/004_user_login/oracle.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.1-6.5.0/Database/004_user_login/oracle.sql
@@ -3,8 +3,8 @@ CREATE TABLE user_login (
   id NUMBER(19, 0) NOT NULL,
   lastConnection NUMBER(19, 0),
   PRIMARY KEY (tenantid, id)
-);
+)@@
 INSERT INTO user_login (tenantid, id, lastConnection)
-SELECT tenantid, id, lastConnection FROM user_;
+SELECT tenantid, id, lastConnection FROM user_@@
 ALTER TABLE user_
-DROP COLUMN lastConnection;
+DROP COLUMN lastConnection@@
