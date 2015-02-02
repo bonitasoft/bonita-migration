fb97b849ac071d55cc6c36e3437ce20fcde3a6ef
Charles Souillard <charles.souillard@bonitasoft.com>
2015-02-02 17:02:11 +0100

    fix drop index statement (missing table name)

--- bonita-migration-distrib/src/main/resources/versions/6.4.1-6.5.0/Database/005_remove_deleted/sqlserver.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.1-6.5.0/Database/005_remove_deleted/sqlserver.sql
@@ -1,4 +1,4 @@
 DELETE FROM flownode_instance WHERE deleted = 1@@
-DROP INDEX idx_fn_lg2_state_tenant_del@@
+DROP INDEX idx_fn_lg2_state_tenant_del ON flownode_instance@@
 CREATE INDEX idx_fn_lg2_state_tenant_del ON flownode_instance (logicalGroup2, stateName, tenantid)@@
 ALTER TABLE flownode_instance DROP COLUMN deleted@@
