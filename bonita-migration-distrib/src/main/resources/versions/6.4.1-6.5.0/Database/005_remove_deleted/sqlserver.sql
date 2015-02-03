e2bf987019fe910768f148e9b9b3da2b146d6177
Charles Souillard <charles.souillard@bonitasoft.com>
2015-02-03 10:03:46 +0100

    clean sqlserver.sql content and add comments to explain the code

--- bonita-migration-distrib/src/main/resources/versions/6.4.1-6.5.0/Database/005_remove_deleted/sqlserver.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.1-6.5.0/Database/005_remove_deleted/sqlserver.sql
@@ -2,6 +2,7 @@ DELETE FROM flownode_instance WHERE deleted = 1@@
 DROP INDEX idx_fn_lg2_state_tenant_del ON flownode_instance@@
 CREATE INDEX idx_fn_lg2_state_tenant_del ON flownode_instance (logicalGroup2, stateName, tenantid)@@
 
+-- SQL Server generates default constraints that must be deleted before we drop the column. The name is noy known so we have to find it...
 -- first define variables
 declare @default sysname, @sql nvarchar(max)
 -- get name of default constraint
@@ -18,6 +19,6 @@ AND parent_column_id = (
 -- create alter table command as string and run it
 set @sql = N'alter table flownode_instance drop constraint ' + @default
 exec sp_executesql @sql
+
 -- now we can finally drop column
---ALTER TABLE flownode_instance DROP CONSTRAINT DF__flownode___delet__5F141958, COLUMN deleted@@
 ALTER TABLE flownode_instance DROP COLUMN deleted@@
