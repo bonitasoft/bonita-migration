e9b55d0eb3d619528508ea61aff6858c5800812e
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-02-12 10:35:03 +0100

    BS-12412, migrate the update to 255 chars off all StringIndex DB fields (for all 4 supported DBs)

---
+++ bonita-migration-distrib/src/main/resources/versions/6.4.2-6.5.0/Database/008_increase_db_text_fields/postgres-update.sql
@@ -0,0 +1,11 @@
+ALTER TABLE process_instance ALTER COLUMN stringIndex1 TYPE VARCHAR(255);
+ALTER TABLE process_instance ALTER COLUMN stringIndex2 TYPE VARCHAR(255);
+ALTER TABLE process_instance ALTER COLUMN stringIndex3 TYPE VARCHAR(255);
+ALTER TABLE process_instance ALTER COLUMN stringIndex4 TYPE VARCHAR(255);
+ALTER TABLE process_instance ALTER COLUMN stringIndex5 TYPE VARCHAR(255);
+
+ALTER TABLE arch_process_instance ALTER COLUMN stringIndex1 TYPE VARCHAR(255);
+ALTER TABLE arch_process_instance ALTER COLUMN stringIndex2 TYPE VARCHAR(255);
+ALTER TABLE arch_process_instance ALTER COLUMN stringIndex3 TYPE VARCHAR(255);
+ALTER TABLE arch_process_instance ALTER COLUMN stringIndex4 TYPE VARCHAR(255);
+ALTER TABLE arch_process_instance ALTER COLUMN stringIndex5 TYPE VARCHAR(255);
