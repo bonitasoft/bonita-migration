a54325a4d93c9d3d3b376d2031a50f5693257a03
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-03-02 12:12:50 +0100

    Fixed migration step now that 6.4.2 - 7.0.0 exists

--- bonita-migration-db-filler-6.5.0/src/main/java/org/bonitasoft/migration/DatabaseChecker6_5_0.java
+++ bonita-migration-db-filler-6.5.0/src/main/java/org/bonitasoft/migration/DatabaseChecker6_5_0.java
@@ -16,40 +16,18 @@ package org.bonitasoft.migration;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-import java.io.Serializable;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
 import org.bonitasoft.engine.api.IdentityAPI;
-import org.bonitasoft.engine.api.TenantAPIAccessor;
-import org.bonitasoft.engine.api.TenantAdministrationAPI;
-import org.bonitasoft.engine.bdm.BusinessObjectModelConverter;
-import org.bonitasoft.engine.bdm.model.BusinessObject;
-import org.bonitasoft.engine.bdm.model.BusinessObjectModel;
-import org.bonitasoft.engine.bdm.model.Query;
-import org.bonitasoft.engine.bdm.model.field.FieldType;
-import org.bonitasoft.engine.bdm.model.field.RelationField;
-import org.bonitasoft.engine.bdm.model.field.SimpleField;
 import org.bonitasoft.engine.api.ProcessAPI;
 import org.bonitasoft.engine.bpm.flownode.FlowNodeInstance;
 import org.bonitasoft.engine.bpm.flownode.FlowNodeInstanceSearchDescriptor;
 import org.bonitasoft.engine.bpm.flownode.HumanTaskInstance;
 import org.bonitasoft.engine.bpm.process.ProcessDefinition;
-import org.bonitasoft.engine.bpm.process.ProcessInstance;
 import org.bonitasoft.engine.bpm.process.impl.ProcessDefinitionBuilder;
-import org.bonitasoft.engine.expression.Expression;
 import org.bonitasoft.engine.expression.ExpressionBuilder;
-import org.bonitasoft.engine.expression.ExpressionEvaluationException;
-import org.bonitasoft.engine.expression.InvalidExpressionException;
 import org.bonitasoft.engine.identity.User;
 import org.bonitasoft.engine.identity.UserNotFoundException;
-import org.bonitasoft.engine.io.IOUtil;
-import org.bonitasoft.engine.operation.OperationBuilder;
-import org.bonitasoft.engine.test.APITestUtil;
 import org.bonitasoft.engine.search.SearchOptionsBuilder;
 import org.bonitasoft.engine.search.SearchResult;
 import org.bonitasoft.engine.test.BuildTestUtil;
@@ -94,9 +72,9 @@ public class DatabaseChecker6_5_0 extends SimpleDatabaseChecker6_5_0 {
 
     @Test
     public void user_should_keep_there_last_connection_date() throws UserNotFoundException {
-        IdentityAPI identityAPI = getApiTestUtil().getIdentityAPI();
-        User userWithLoginDate = identityAPI.getUserByUserName("userWithLoginDate");
-        User userWithoutLoginDate = identityAPI.getUserByUserName("userWithoutLoginDate");
+        final IdentityAPI identityAPI = getApiTestUtil().getIdentityAPI();
+        final User userWithLoginDate = identityAPI.getUserByUserName("userWithLoginDate");
+        final User userWithoutLoginDate = identityAPI.getUserByUserName("userWithoutLoginDate");
         assertThat(userWithLoginDate.getLastConnection()).isBefore(new Date());
         assertThat(userWithoutLoginDate.getLastConnection()).isNull();
 
@@ -106,11 +84,10 @@ public class DatabaseChecker6_5_0 extends SimpleDatabaseChecker6_5_0 {
     public void should_deleted_exists_anymore() throws Exception {
         final ProcessAPI processAPI = getApiTestUtil().getProcessAPI();
         final long processId = processAPI.getProcessDefinitionId("SimpleProcessWithDeleted", "1.0");
-        final SearchResult<FlowNodeInstance> flowNodeInstanceSearchResult = processAPI.searchFlowNodeInstances(new SearchOptionsBuilder(0, 10).filter(FlowNodeInstanceSearchDescriptor.PROCESS_DEFINITION_ID, processId).done());
+        final SearchResult<FlowNodeInstance> flowNodeInstanceSearchResult = processAPI.searchFlowNodeInstances(new SearchOptionsBuilder(0, 10).filter(
+                FlowNodeInstanceSearchDescriptor.PROCESS_DEFINITION_ID, processId).done());
         assertThat(flowNodeInstanceSearchResult.getCount()).isEqualTo(1);
         assertThat(flowNodeInstanceSearchResult.getResult().get(0).getName()).isEqualTo("human");
     }
-
-
 
 }
