50a5a6bf2ca81031d10bd259d2227f9eb7d2acf7
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-03-03 18:53:51 +0100

    S-6831 : Oracle, "MODIFY column" instruction does not need to re-specify nullability

--- bonita-migration-distrib/src/main/resources/versions/6.4.2-6.5.0/Database/002_oracle_varchar2_byte/oracle-message_instance.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.2-6.5.0/Database/002_oracle_varchar2_byte/oracle-message_instance.sql
@@ -3,7 +3,7 @@
 -- 
 ALTER TABLE message_instance MODIFY messageName VARCHAR2(255 CHAR) @@
 ALTER TABLE message_instance MODIFY targetProcess VARCHAR2(255 CHAR) @@
-ALTER TABLE message_instance MODIFY targetFlowNode VARCHAR2(255 CHAR) NULL @@
+ALTER TABLE message_instance MODIFY targetFlowNode VARCHAR2(255 CHAR) @@
 ALTER TABLE message_instance MODIFY flowNodeName VARCHAR2(255 CHAR) @@
 ALTER TABLE message_instance MODIFY correlation1 VARCHAR2(128 CHAR) @@
 ALTER TABLE message_instance MODIFY correlation2 VARCHAR2(128 CHAR) @@
