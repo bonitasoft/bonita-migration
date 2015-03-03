6252b8395751112a4f10c6d1d99e4a3047a9814c
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-03-03 17:37:42 +0100

    BS-6831 : Oracle, try to change "MODIFY column" order

--- bonita-migration-distrib/src/main/resources/versions/6.4.2-6.5.0/Database/002_oracle_varchar2_byte/oracle-data_instance.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.2-6.5.0/Database/002_oracle_varchar2_byte/oracle-data_instance.sql
@@ -6,8 +6,8 @@ ALTER TABLE data_instance MODIFY name VARCHAR2(50 CHAR) @@
 ALTER TABLE data_instance MODIFY description VARCHAR2(50 CHAR) @@
 ALTER TABLE data_instance MODIFY className VARCHAR2(100 CHAR) @@
 ALTER TABLE data_instance MODIFY containerType VARCHAR2(60 CHAR) @@
-ALTER TABLE data_instance MODIFY namespace VARCHAR2(100 CHAR) @@
 ALTER TABLE data_instance MODIFY element VARCHAR2(60 CHAR) @@
+ALTER TABLE data_instance MODIFY namespace VARCHAR2(100 CHAR) @@
 ALTER TABLE data_instance MODIFY shortTextValue VARCHAR2(255 CHAR) @@
 ALTER TABLE data_instance MODIFY discriminant VARCHAR2(50 CHAR) @@
 
