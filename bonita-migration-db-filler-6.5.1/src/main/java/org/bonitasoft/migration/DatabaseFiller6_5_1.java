62d36e6cf636aaf770120967becdefd9a2239215
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2015-04-01 16:58:54 +0200

    BS-12863, add migration step to update 'stable' flag of active event sub-processes

--- bonita-migration-db-filler-6.5.1/src/main/java/org/bonitasoft/migration/DatabaseFiller6_5_1.java
+++ bonita-migration-db-filler-6.5.1/src/main/java/org/bonitasoft/migration/DatabaseFiller6_5_1.java
@@ -13,12 +13,101 @@
  **/
 package org.bonitasoft.migration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+
+import org.bonitasoft.engine.bpm.flownode.ActivityInstance;
+import org.bonitasoft.engine.bpm.process.ProcessDefinition;
+import org.bonitasoft.engine.bpm.process.ProcessInstance;
+import org.bonitasoft.engine.bpm.process.impl.ProcessDefinitionBuilder;
+import org.bonitasoft.engine.bpm.process.impl.StartEventDefinitionBuilder;
+import org.bonitasoft.engine.bpm.process.impl.SubProcessDefinitionBuilder;
+import org.bonitasoft.engine.expression.InvalidExpressionException;
+import org.bonitasoft.engine.test.ClientEventUtil;
+import org.bonitasoft.engine.test.TestStates;
+
 public class DatabaseFiller6_5_1 extends DatabaseFiller6_4_2 {
+
+    public static final String SUB_PROCESS_START_NAME = "subProcessStart";
+
+    public static final String SUB_PROCESS_USER_TASK_NAME = "subStep";
+
+    public static final String SUB_PROCESS_NAME = "eventSubProcess";
+
+    public static final String PARENT_PROCESS_USER_TASK_NAME = "step1";
+
+    public static final String PARENT_END = "end";
+
+    public static final String ACTOR_NAME = "delivery";
+
+    public static final String SIGNAL_NAME = "canStart";
+
+    public static final String PROCESS_WITH_EVENT_SUB_PROCESS = "ProcessWithEventSubProcess";
 
     public static void main(final String[] args) throws Exception {
         DatabaseFiller6_5_1 databaseFiller = new DatabaseFiller6_5_1();
         databaseFiller.execute(1, 1, 1, 1);
     }
-    
+
+    @Override
+    public Map<String, String> fillDatabase(final int nbProcessesDefinitions, final int nbProcessInstances, final int nbWaitingEvents, final int nbDocuments) throws Exception {
+        Map<String, String> stats = super.fillDatabase(nbProcessesDefinitions, nbProcessInstances, nbWaitingEvents, nbDocuments);
+        apiTestUtil.loginOnDefaultTenantWithDefaultTechnicalUser();
+        apiTestUtil.loginOnDefaultTenantWithDefaultTechnicalUser();
+        ClientEventUtil.deployCommand(apiTestUtil.getSession());
+
+        startProcessWithSignalEventSubProcess();
+
+        ClientEventUtil.undeployCommand(apiTestUtil.getSession());
+        apiTestUtil.logoutOnTenant();
+        return stats;
+    }
+
+    public void startProcessWithSignalEventSubProcess() throws Exception {
+        // given
+        final ProcessDefinition process = apiTestUtil.deployAndEnableProcessWithActor(buildSubProcessDefinition(buildParentProcessDefinition()).done(),
+                ACTOR_NAME, apiTestUtil.getIdentityAPI().getUserByUserName("william.jobs"));
+        final ProcessInstance processInstance = apiTestUtil.getProcessAPI().startProcess(process.getId());
+        apiTestUtil.waitForFlowNodeInReadyState(processInstance, PARENT_PROCESS_USER_TASK_NAME, true);
+
+        // when
+        apiTestUtil.getProcessAPI().sendSignal(SIGNAL_NAME);
+
+        // then
+        apiTestUtil.waitForFlowNodeInState(processInstance, PARENT_PROCESS_USER_TASK_NAME, TestStates.ABORTED, true);
+        apiTestUtil.waitForFlowNodeInReadyState(processInstance, SUB_PROCESS_USER_TASK_NAME, true);
+
+        final List<ActivityInstance> activities = apiTestUtil.getProcessAPI().getActivities(processInstance.getId(), 0, 10);
+        assertThat(activities).as("should have 2 activities: sub-process flow node and user task").hasSize(2);
+        assertThat(activities.get(0).getName()).isEqualTo(SUB_PROCESS_NAME);
+        assertThat(activities.get(1).getName()).isEqualTo(SUB_PROCESS_USER_TASK_NAME);
+
+        // execute migration
+    }
+
+    public ProcessDefinitionBuilder buildParentProcessDefinition()
+            throws InvalidExpressionException {
+        final ProcessDefinitionBuilder builder = new ProcessDefinitionBuilder().createNewInstance(PROCESS_WITH_EVENT_SUB_PROCESS, PROCESS_VERSION);
+        builder.addActor(ACTOR_NAME);
+        builder.addStartEvent("start");
+        builder.addUserTask(PARENT_PROCESS_USER_TASK_NAME, ACTOR_NAME);
+        builder.addEndEvent(PARENT_END);
+        builder.addTransition("start", PARENT_PROCESS_USER_TASK_NAME);
+        builder.addTransition(PARENT_PROCESS_USER_TASK_NAME, PARENT_END);
+        return builder;
+    }
+
+    public ProcessDefinitionBuilder buildSubProcessDefinition(final ProcessDefinitionBuilder builder) throws InvalidExpressionException {
+        final SubProcessDefinitionBuilder subProcessBuilder = builder.addSubProcess(SUB_PROCESS_NAME, true).getSubProcessBuilder();
+        final StartEventDefinitionBuilder startEventDefinitionBuilder = subProcessBuilder.addStartEvent(SUB_PROCESS_START_NAME);
+        startEventDefinitionBuilder.addSignalEventTrigger(SIGNAL_NAME);
+        subProcessBuilder.addUserTask(SUB_PROCESS_USER_TASK_NAME, ACTOR_NAME);
+        subProcessBuilder.addEndEvent("endSubProcess");
+        subProcessBuilder.addTransition(SUB_PROCESS_START_NAME, SUB_PROCESS_USER_TASK_NAME);
+        subProcessBuilder.addTransition(SUB_PROCESS_USER_TASK_NAME, "endSubProcess");
+        return builder;
+    }
 
 }
--- bonita-migration-db-filler-6.5.2/src/main/java/org/bonitasoft/migration/DatabaseChecker6_5_2.java
+++ bonita-migration-db-filler-6.5.2/src/main/java/org/bonitasoft/migration/DatabaseChecker6_5_2.java
@@ -13,12 +13,30 @@
  **/
 package org.bonitasoft.migration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.bonitasoft.engine.bpm.flownode.ActivityInstance;
+import org.bonitasoft.engine.bpm.process.ProcessInstance;
+import org.bonitasoft.engine.bpm.process.ProcessInstanceSearchDescriptor;
+import org.bonitasoft.engine.bpm.process.ProcessInstanceState;
+import org.bonitasoft.engine.exception.SearchException;
+import org.bonitasoft.engine.search.SearchOptionsBuilder;
+import org.bonitasoft.engine.search.SearchResult;
 import org.dom4j.Document;
 import org.dom4j.io.SAXReader;
+import org.junit.Test;
 import org.junit.runner.JUnitCore;
 
 
 public class DatabaseChecker6_5_2 extends SimpleDatabaseChecker6_5_0 {
+
+    public static final String SUB_PROCESS_USER_TASK_NAME = "subStep";
+
+    public static final String SUB_PROCESS_NAME = "eventSubProcess";
+
+    public static final String PROCESS_WITH_EVENT_SUB_PROCESS = "ProcessWithEventSubProcess";
     
     public static void main(final String[] args) throws Exception {
         JUnitCore.main(DatabaseChecker6_5_2.class.getName());
@@ -29,5 +47,39 @@ public class DatabaseChecker6_5_2 extends SimpleDatabaseChecker6_5_0 {
         return reader.read(DatabaseChecker6_5_2.class.getResource("profiles.xml"));
     }
 
+    @Test
+    public void event_sub_process_should_be_available_after_migration() throws Exception {
+        getApiTestUtil().loginOnDefaultTenantWithDefaultTechnicalUser();
 
+        //given
+        long definitionId = getApiTestUtil().getProcessAPI().getProcessDefinitionId(PROCESS_WITH_EVENT_SUB_PROCESS, SimpleDatabaseFiller6_0_2.PROCESS_VERSION);
+        SearchResult<ProcessInstance> searchResult = getRootProcessInstancesOfDefinition(definitionId);
+        assertThat(searchResult.getResult()).hasSize(1);
+
+        //when
+        ProcessInstance processInstance = searchResult.getResult().get(0);
+        final List<ActivityInstance> activities = getApiTestUtil().getProcessAPI().getActivities(processInstance.getId(), 0, 10);
+
+        //then: task should be available after migration
+        assertThat(activities).as("should have 2 activities: sub-process flow node and user task").hasSize(2);
+        assertThat(activities.get(0).getName()).isEqualTo(SUB_PROCESS_NAME);
+        ActivityInstance subProcUserTask = activities.get(1);
+        assertThat(subProcUserTask.getName()).isEqualTo(SUB_PROCESS_USER_TASK_NAME);
+
+        ProcessInstance subProcessInstance = getApiTestUtil().getProcessAPI().getProcessInstance(subProcUserTask.getParentProcessInstanceId());
+        getApiTestUtil().assignAndExecuteStep(subProcUserTask, getApiTestUtil().getIdentityAPI().getUserByUserName("william.jobs"));
+        getApiTestUtil().waitForActivityInCompletedState(processInstance, SUB_PROCESS_USER_TASK_NAME, true);
+        getApiTestUtil().waitForProcessToFinish(subProcessInstance);
+        getApiTestUtil().waitForActivityInCompletedState(processInstance, SUB_PROCESS_NAME, true);
+        getApiTestUtil().waitForProcessToBeInState(processInstance, ProcessInstanceState.ABORTED);
+
+        getApiTestUtil().logoutOnTenant();
+    }
+
+    protected SearchResult<ProcessInstance> getRootProcessInstancesOfDefinition(final long processDefinitionId) throws SearchException {
+        final SearchOptionsBuilder builder = new SearchOptionsBuilder(0, 100);
+        builder.filter(ProcessInstanceSearchDescriptor.PROCESS_DEFINITION_ID, processDefinitionId);
+        builder.filter(ProcessInstanceSearchDescriptor.CALLER_ID, -1);
+        return getProcessAPI().searchProcessInstances(builder.done());
+    }
 }
