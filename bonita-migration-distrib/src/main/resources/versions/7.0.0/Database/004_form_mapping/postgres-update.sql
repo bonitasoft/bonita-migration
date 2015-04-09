43cd1fc731c994809fadce6d1c8909d207de0594
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-04-09 13:49:14 +0200

    BS-12888, fixed migration SQL scripts

--- bonita-migration-distrib/src/main/resources/versions/7.0.0/Database/004_form_mapping/postgres-update.sql
+++ bonita-migration-distrib/src/main/resources/versions/7.0.0/Database/004_form_mapping/postgres-update.sql
@@ -2,7 +2,3 @@ INSERT INTO sequence VALUES(:tenantId, 10300, 1)
 @@
 INSERT INTO sequence VALUES(:tenantId, 10121, 1)
 @@
-INSERT INTO sequence VALUES(:tenantId, 10220, 1)
-@@
-INSERT INTO sequence VALUES(:tenantId, 20220, 1)
-@@
