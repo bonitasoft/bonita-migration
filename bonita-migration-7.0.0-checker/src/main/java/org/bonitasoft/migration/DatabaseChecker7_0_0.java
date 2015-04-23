69a1c06b1d7b76b4f000872aa98c8f0f088a91ab
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-04-23 14:52:56 +0200

    migrate bonita home to the new structure

--- bonita-migration-7.0.0-checker/src/main/java/org/bonitasoft/migration/DatabaseChecker7_0_0.java
+++ bonita-migration-7.0.0-checker/src/main/java/org/bonitasoft/migration/DatabaseChecker7_0_0.java
@@ -16,6 +16,7 @@ package org.bonitasoft.migration;
 import org.bonitasoft.engine.identity.User;
 import org.dom4j.Document;
 import org.dom4j.io.SAXReader;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 
@@ -25,17 +26,6 @@ public class DatabaseChecker7_0_0 extends SimpleDatabaseChecker7_0_0 {
         JUnitCore.main(DatabaseChecker7_0_0.class.getName());
     }
 
-    @Override
-    protected Document getProfilesXML(final SAXReader reader) throws Exception {
-        return reader.read(DatabaseChecker7_0_0.class.getResource("profiles.xml"));
-    }
-
-    @Override
-    // This method is because there is no need to check profile migration anymore, as they are updated at Engine restart.
-    public void check_profiles() throws Exception {
-        System.out.println("Profiles do not need to be migrated anymore (from 7.0.0 on). Nothing to check here.");
-    }
-
     @Test
     public void should_deploy_a_business_data_model() throws Exception {
         final User user = getApiTestUtil().createUser("matti", "bpm");
@@ -43,8 +33,4 @@ public class DatabaseChecker7_0_0 extends SimpleDatabaseChecker7_0_0 {
         getIdentityApi().deleteUser(user.getId());
     }
 
-    @Override
-    public void commands_are_up_to_date() throws Exception {
-        //no check, auto update
-    }
 }
--- bonita-migration-distrib/src/main/resources/versions/7.0.0/Bonita-home/MigrateServerBonitaHome.groovy
+++ bonita-migration-distrib/src/main/resources/versions/7.0.0/Bonita-home/MigrateServerBonitaHome.groovy
@@ -1,40 +1,44 @@
 import org.bonitasoft.migration.core.IOUtil
-import org.bonitasoft.migration.core.MigrationUtil
 
-def currentDir = ""
+import java.nio.file.Path
 
 def File newBonitaHome = new File(newBonitaHome, "/engine-server/")
 def File newBonitaHomeDestination = new File(bonitaHome, "/engine-server/")
 def File bonitaHomeToMigrate = new File(bonitaHome, "/server/")
 
-IOUtil.copyDirectory(bonitaHomeToMigrate, newBonitaHomeDestination)
-IOUtil.deleteDirectory(bonitaHomeToMigrate)
-bonitaHomeToMigrate = newBonitaHomeDestination
-MigrationUtil.migrateDirectory(newBonitaHome.path + currentDir, bonitaHomeToMigrate.path + currentDir, true)
+//Copy original engine-server
+IOUtil.copyDirectory(newBonitaHome, newBonitaHomeDestination);
+IOUtil.copyDirectory(bonitaHomeToMigrate, new File(bonitaHome, "server_6.x") );
 
 
+move(new File(bonitaHomeToMigrate.path + "/platform/work"), new File(newBonitaHomeDestination.path + "/work/platform"))
+def tenantsFolder = new File(bonitaHomeToMigrate, "tenants")
+tenantsFolder.listFiles().each { tenantFolder ->
+    def tenantId = Long.valueOf(tenantFolder.getName())
+    println "migrating tenant: " + tenantId
+    //MOVE:  server/tenants/X/work/processes -> engine-server/work/tenants/X/processes
+    move(new File(tenantFolder.path + "/work/processes"), new File(newBonitaHomeDestination.path + "/work/tenants/$tenantId/processes"))
+    //MOVE:  server/tenants/X/work/security-scripts-> engine-server/work/tenants/X/security-scripts
+    move(new File(tenantFolder.path + "/work/security-scripts"), new File(newBonitaHomeDestination.path + "/work/tenants/$tenantId/security-scripts"))
+    //Create:  X -> engine-server/work/tenants/X/bonita-tenant-id.properties that contains tenantId=X
 
-currentDir = "/platform/conf"
-MigrationUtil.migrateDirectory(newBonitaHome.path + currentDir, bonitaHomeToMigrate.path + currentDir, true)
-
-currentDir = "/platform/tenant-template"
-MigrationUtil.migrateDirectory(newBonitaHome.path + currentDir, bonitaHomeToMigrate.path + currentDir, true)
-
-println "Detecting tenants..."
-def tenantsServerDir = new File(bonitaHomeToMigrate, "/tenants")
-if (tenantsServerDir.exists()) {
-    def tenants = Arrays.asList(tenantsServerDir.listFiles());
-    if (tenants.empty) {
-        println "No tenant found."
-    } else {
-        println "Executing update for each tenant : " + tenants;
-        tenantsServerDir.eachFile { tenant ->
-            println "For tenant : " + tenant.name
-            IOUtil.executeWrappedWithTabs {
-                MigrationUtil.migrateDirectory(newBonitaHome.path + "/platform/tenant-template/conf", tenant.path + "/conf", true)
-            }
-        }
-    }
-} else {
-    println "No tenant found."
+    def tenantProperties = newBonitaHomeDestination.path + "/work/tenants/$tenantId/bonita-tenant-id.properties"
+    println "Write file $tenantProperties"
+    new File(tenantProperties).write("tenantId=$tenantId")
+    //Copy:  X -> engine-server/work/tenants/template/* -> engine-server/work/tenants/X/.
+    IOUtil.copyDirectory(new File(newBonitaHomeDestination.path + "/work/tenants/template"), new File(newBonitaHomeDestination.path + "/work/tenants/$tenantId"));
+    //Copy:  X -> engine-server/conf/tenants/template/* -> engine-server/conf/tenants/X/.
+    IOUtil.copyDirectory(new File(newBonitaHomeDestination.path + "/conf/tenants/template"), new File(newBonitaHomeDestination.path + "/conf/tenants/$tenantId"));
 }
+
+def move(File src, File dest) {
+    if (src.exists()) {
+        println "Move $src to $dest"
+        IOUtil.copyDirectory(src, dest)
+        IOUtil.deleteDirectory(src)
+    }else {
+        println "src file $src does not exists"
+    }
+}
+
+IOUtil.deleteDirectory(bonitaHomeToMigrate)
