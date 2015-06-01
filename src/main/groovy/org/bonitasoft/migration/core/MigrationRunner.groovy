43458fae6627ee129829617644fe63aca6e9c06b
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-06-01 17:24:43 +0200

    first structure to have an application using gradle build

---
+++ src/main/groovy/org/bonitasoft/migration/core/MigrationRunner.groovy
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2015 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+
+package org.bonitasoft.migration.core
+
+/**
+ * @author Baptiste Mesta
+ */
+class MigrationRunner {
+
+    def execute(){
+        println ''
+        IOUtil.printInRectangle("", "Bonita BPM migration tool", "",
+                "This tool will migrate your installation of Bonita BPM.",
+                "Both database and bonita home will be modified.",
+                "Please refer to the documentation for further steps to completely migrate your production environment.",
+                "",
+                "Warning:",
+                "Back up the database AND the bonita home before migrating",
+                "If you have a custom Look & Feel, test and update it, if it's necessary when the migration is finished.",
+                "If you have customized the configuration of your bonita home, reapply the customizations when the migration is finished.", "")
+        IOUtil.askIfWeContinue();
+    }
+}
