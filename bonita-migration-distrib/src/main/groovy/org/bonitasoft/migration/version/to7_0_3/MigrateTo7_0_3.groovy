a32c28f603b409e3a75b33da036e6e1518b4d6e0
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-08-10 15:19:47 +0200

    add step 7.0.3 in the migration tool itself

---
+++ bonita-migration-distrib/src/main/groovy/org/bonitasoft/migration/version/to7_0_3/MigrateTo7_0_3.groovy
@@ -0,0 +1,14 @@
+package org.bonitasoft.migration.version.to7_0_3
+
+import org.bonitasoft.migration.core.MigrationStep
+import org.bonitasoft.migration.core.VersionMigration
+
+/**
+ * @author Baptiste Mesta
+ */
+class MigrateTo7_0_3 extends VersionMigration {
+    @Override
+    def List<MigrationStep> getMigrationSteps() {
+        return []
+    }
+}
