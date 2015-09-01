3522afca3f1284f3a6f7f32a63fd31764fc6ec79
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-09-01 11:15:47 +0200

    update copyright

--- bonita-migration-distrib/src/test/groovy/org/bonitasoft/migration/core/MigrationRunnerTest.groovy
+++ bonita-migration-distrib/src/test/groovy/org/bonitasoft/migration/core/MigrationRunnerTest.groovy
@@ -1,6 +1,22 @@
+/**
+ * Copyright (C) 2015 Bonitasoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+
 package org.bonitasoft.migration.core
+
 import groovy.sql.Sql
 import org.junit.Test
+
 /**
  * @author Baptiste Mesta
  */
@@ -8,19 +24,19 @@ class MigrationRunnerTest {
 
     def infos = []
     def sql = [] as Sql
-    def logger = [info:{ String message -> infos.add(message)}] as Logger
+    def logger = [info: { String message -> infos.add(message) }] as Logger
     def boolean isStepExecuted = false
 
     @Test
     void testRun() {
 
-        def migrations = [new VersionMigration(){
+        def migrations = [new VersionMigration() {
 
             @Override
             List<MigrationStep> getMigrationSteps() {
                 return [new MigrationStep() {
                     @Override
-                    def execute(Sql sql, MigrationStep.DBVendor dbVendor) {
+                    def execute(Sql sql, DBVendor dbVendor) {
                         isStepExecuted = true
                         return null
                     }
