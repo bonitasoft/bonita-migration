e3a8c6ecee68121da51db6527037357460a108a4
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2015-09-02 15:30:01 +0200

    BS-14238, fix "kind" default value as expected by engine

--- bonita-migration-distrib/src/main/resources/versions/6.4.0/Database/002_business_data/postgres-update.sql
+++ bonita-migration-distrib/src/main/resources/versions/6.4.0/Database/002_business_data/postgres-update.sql
@@ -11,7 +11,7 @@ CREATE TABLE ref_biz_data_inst2 (
 @@
 
 INSERT INTO ref_biz_data_inst2(tenantid, id, name, data_id, proc_inst_id, fn_inst_id, data_classname, kind)
-SELECT r.tenantid, r.id, r.name, r.data_id, p.id, NULL, r.data_classname, 'simple_ref'
+SELECT r.tenantid, r.id, r.name, r.data_id, p.id, NULL, r.data_classname, 'proc_simple_ref'
 FROM ref_biz_data_inst r left join PROCESS_INSTANCE p on  r.PROC_INST_ID=p.ID and r.TENANTID=p.TENANTID
 WHERE p.ID is not null
 @@
