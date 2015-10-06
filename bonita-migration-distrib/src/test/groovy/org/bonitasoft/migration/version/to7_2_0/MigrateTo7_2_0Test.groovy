37f855c01728016e71ce6614b6de19eb1a48d8b0
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2015-10-06 18:28:10 +0200

    BS-14357, fix sp unit test

--- bonita-migration-distrib/src/test/groovy/org/bonitasoft/migration/version/to7_2_0/MigrateTo7_2_0Test.groovy
+++ bonita-migration-distrib/src/test/groovy/org/bonitasoft/migration/version/to7_2_0/MigrateTo7_2_0Test.groovy
@@ -1,20 +1,16 @@
 package org.bonitasoft.migration.version.to7_2_0
-
 import spock.lang.Specification
-import spock.lang.Unroll
-
 /**
  * @author Elias Ricken de Medeiros
  */
-@Unroll
 class MigrateTo7_2_0Test extends Specification {
 
     def "should contains #step"() {
+        setup:
         def migration = new MigrateTo7_2_0();
 
         expect:
         migration.getMigrationSteps().size() == 0
-
     }
 
 }
