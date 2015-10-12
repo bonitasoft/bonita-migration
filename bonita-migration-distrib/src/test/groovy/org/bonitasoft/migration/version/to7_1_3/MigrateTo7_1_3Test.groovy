6d968664b62f6b2bdab19a2a8b078c094a830a98
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-10-12 12:05:04 +0200

    BS-13432, add missing index on arch_flownode_instance (tenantId, kind, logicalGroup3) to improve search performance

--- bonita-migration-distrib/src/test/groovy/org/bonitasoft/migration/version/to7_1_3/MigrateTo7_1_3Test.groovy
+++ bonita-migration-distrib/src/test/groovy/org/bonitasoft/migration/version/to7_1_3/MigrateTo7_1_3Test.groovy
@@ -1,21 +1,24 @@
 package org.bonitasoft.migration.version.to7_1_3
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * @author Laurent Leseigneur
  */
+@Unroll
 class MigrateTo7_1_3Test extends Specification {
 
     def "should contains #step"() {
         setup:
         def migrateTo7_1_3 = new MigrateTo7_1_3()
 
-        when:
-        def steps = migrateTo7_1_3.migrationSteps
+        expect:
+        migrateTo7_1_3.getMigrationSteps().get(index).getClass() == step
 
-        then:
-        steps.size() == 1
+        where:
+        index || step
+        0     || MigrateArchFlowNodeInstanceIndex
 
     }
 
