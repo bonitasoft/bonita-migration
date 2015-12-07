2557f65473e89352cc4fd9910780c54b1a96a1a6
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-12-07 16:00:28 +0100

    replace ; by @@ separator for scripts

--- bonita-migration-distrib/src/main/resources/version/to_7_2_0/BARInDatabase/mysql_.sql
+++ bonita-migration-distrib/src/main/resources/version/to_7_2_0/BARInDatabase/mysql_.sql
@@ -7,5 +7,6 @@ CREATE TABLE bar_resource (
   content LONGBLOB NOT NULL,
   UNIQUE (tenantId, process_id, name, type),
   PRIMARY KEY (tenantId, id)
-) ENGINE = INNODB;
-CREATE INDEX idx_bar_resource ON bar_resource (tenantId, process_id, type, name);
+) ENGINE = INNODB
+@@
+CREATE INDEX idx_bar_resource ON bar_resource (tenantId, process_id, type, name)
--- bonita-migration-distrib/src/main/resources/version/to_7_2_0/BARInDatabase/postgres_.sql
+++ bonita-migration-distrib/src/main/resources/version/to_7_2_0/BARInDatabase/postgres_.sql
@@ -7,5 +7,6 @@ CREATE TABLE bar_resource (
   content BYTEA NOT NULL,
   UNIQUE (tenantId, process_id, name, type),
   PRIMARY KEY (tenantId, id)
-);
-CREATE INDEX idx_bar_resource ON bar_resource (tenantId, process_id, type, name);
+)
+@@
+CREATE INDEX idx_bar_resource ON bar_resource (tenantId, process_id, type, name)
