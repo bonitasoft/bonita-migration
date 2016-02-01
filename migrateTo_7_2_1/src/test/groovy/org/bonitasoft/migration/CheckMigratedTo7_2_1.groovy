daf456425802e37b7d52fe02d1a9292750993bb1
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2016-02-01 12:07:33 +0100

    Added migration step to migrate to 7.2.1

---
+++ migrateTo_7_2_1/src/test/groovy/org/bonitasoft/migration/CheckMigratedTo7_2_1.groovy
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2016 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+
+package org.bonitasoft.migration
+
+import org.bonitasoft.engine.test.junit.BonitaEngineRule
+import org.bonitasoft.migration.filler.FillerUtils
+import org.junit.BeforeClass
+import org.junit.Rule
+
+/**
+ * @author Laurent Leseigneur
+ */
+class CheckMigratedTo7_2_1 {
+
+    @Rule
+    public BonitaEngineRule bonitaEngineRule = BonitaEngineRule.create().reuseExistingPlatform()
+
+
+    @BeforeClass
+    public static void beforeClass() {
+        FillerUtils.initializeEngineSystemProperties()
+    }
+
+
+}
