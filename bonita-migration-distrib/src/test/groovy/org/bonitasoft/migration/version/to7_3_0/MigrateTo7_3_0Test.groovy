0aed5d26527af3ab90858f6bade6be3486d02497
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2016-02-02 09:29:44 +0100

    fix build migrateTo_7_3_0

---
+++ bonita-migration-distrib/src/test/groovy/org/bonitasoft/migration/version/to7_3_0/MigrateTo7_3_0Test.groovy
@@ -0,0 +1,35 @@
+package org.bonitasoft.migration.version.to7_3_0
+
+import spock.lang.Specification
+import spock.lang.Unroll
+/**
+ * @author Emmanuel Duchastenier
+ */
+class MigrateTo7_3_0Test extends Specification {
+
+    @Unroll
+    def "should contains #size migration steps"(def size) {
+        setup:
+        def migration = new MigrateTo7_3_0();
+
+        expect:
+        migration.getMigrationSteps().size() == size
+
+        where:
+        size << 0
+    }
+
+//    @Unroll
+//    def "should contains #step"(def index, def step) {
+//        setup:
+//        def migration = new MigrateTo7_2_0();
+//
+//        expect:
+//        migration.getMigrationSteps().get(index).getClass() == step
+//
+//        where:
+//        index || step
+//        0     || MigrateProcessDefXml
+//    }
+
+}
