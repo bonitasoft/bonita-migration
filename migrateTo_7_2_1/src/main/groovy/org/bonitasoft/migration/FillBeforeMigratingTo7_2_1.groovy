8501383a8c52b859b40533c77140f4e80da95a4e
Baptiste Mesta <baptiste.mesta@gmail.com>
2016-02-12 11:52:16 +0100

    BS-14873, remove unused sequence on contract input

--- migrateTo_7_2_1/src/main/groovy/org/bonitasoft/migration/FillBeforeMigratingTo7_2_1.groovy
+++ migrateTo_7_2_1/src/main/groovy/org/bonitasoft/migration/FillBeforeMigratingTo7_2_1.groovy
@@ -13,8 +13,16 @@
  **/
 package org.bonitasoft.migration
 
-import org.bonitasoft.engine.api.PlatformAPIAccessor
 import org.bonitasoft.engine.test.TestEngineImpl
+import org.bonitasoft.engine.api.APIClient
+import org.bonitasoft.engine.api.PlatformAPIAccessor
+import org.bonitasoft.engine.bpm.bar.BusinessArchiveBuilder
+import org.bonitasoft.engine.bpm.bar.actorMapping.Actor
+import org.bonitasoft.engine.bpm.bar.actorMapping.ActorMapping
+import org.bonitasoft.engine.bpm.contract.Type
+import org.bonitasoft.engine.bpm.flownode.ActivityInstanceCriterion
+import org.bonitasoft.engine.bpm.process.impl.ProcessDefinitionBuilder
+import org.bonitasoft.migration.filler.FillAction
 import org.bonitasoft.migration.filler.FillerInitializer
 import org.bonitasoft.migration.filler.FillerShutdown
 import org.bonitasoft.migration.filler.FillerUtils
@@ -43,4 +51,45 @@ class FillBeforeMigratingTo7_2_1 {
         PlatformAPIAccessor.getPlatformLoginAPI().logout(session)
     }
 
+    @FillAction
+    public void addProcessWithContractInput() {
+        def client = new APIClient()
+        client.login("install", "install")
+        def user = client.identityAPI.createUser("userForContractInput", "bpm")
+        client.logout()
+        client.login("userForContractInput", "bpm")
+
+        def builder = new ProcessDefinitionBuilder().createNewInstance("ProcessWithContractInput", "7.2.0")
+        builder.addAutomaticTask("auto1")
+        builder.addAutomaticTask("auto2")
+        builder.addAutomaticTask("auto3")
+        builder.addAutomaticTask("auto4")
+        builder.addUserTask("step1", "myActor").addContract().addInput("taskInput", Type.BOOLEAN, "task input")
+        builder.addUserTask("step2", "myActor").addContract()
+        builder.addTransition("step1", "step2")
+        builder.addActor("myActor")
+        def contract = builder.addContract()
+        contract.addInput("processInput", Type.BOOLEAN, "process input")
+
+        def mapping = new ActorMapping()
+        def actor = new Actor("myActor")
+        actor.addUser("userForContractInput")
+        mapping.addActor(actor)
+        def businessArchive = new BusinessArchiveBuilder().createNewBusinessArchive().setActorMapping(mapping).setProcessDefinition(builder.done()).done()
+
+        def processDefinition = client.processAPI.deploy(businessArchive)
+        client.processAPI.enableProcess(processDefinition.id)
+
+        client.processAPI.startProcessWithInputs(processDefinition.id, [processInput: true])
+        def instances
+
+        def timeout = System.currentTimeMillis() + 3000
+        while ((instances = client.processAPI.getPendingHumanTaskInstances(user.id, 0, 10, ActivityInstanceCriterion.DEFAULT)).size() < 1 && System.currentTimeMillis() < timeout) {
+            Thread.sleep(200)
+            println "wait 200"
+        }
+        client.processAPI.assignUserTask(instances.get(0).id, user.id)
+        client.processAPI.executeUserTask(instances.get(0).id, [taskInput: true])
+
+    }
 }
