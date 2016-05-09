6e4e89f3adfb64fa889644a624560e4681926c54
Baptiste Mesta <baptiste.mesta@gmail.com>
2016-05-09 16:38:58 +0200

    feat(tenant_resources): migrate client bdm zip in database (#44)
    put the zip in database and remove it from the bonita home
    
    Closes[14943](https://bonitasoft.atlassian.net/browse/BS-14943)

--- bonita-migration-distrib/src/main/groovy/org/bonitasoft/migration/version/to7_3_0/TenantResources.groovy
+++ bonita-migration-distrib/src/main/groovy/org/bonitasoft/migration/version/to7_3_0/TenantResources.groovy
@@ -25,10 +25,22 @@ class TenantResources extends MigrationStep {
         tenants.eachFileMatch DIRECTORIES, ~/[0-9]+/, { tenant ->
             long tenantId = Long.valueOf(tenant.getName())
             resourcesCount.put(tenantId, 1)
-           //migrate tenant resources here
+            migrateClientBDMZip(context, resourcesCount, tenant, tenantId)
         }
         insertSequences(resourcesCount, context)
 
+    }
+
+    void migrateClientBDMZip(MigrationContext context, Map<Long, Long> resourcesCount, File tenantFolder, long tenantId) {
+        def clientBDMZip = new File(new File(tenantFolder, "data-management-client"), "client-bdm.zip")
+        if (!clientBDMZip.exists()) {
+            return
+        }
+        def long resourceId = resourcesCount.get(tenantId)
+        context.logger.info("Put client bdm zip in database with id $resourceId for tenant $tenantId")
+        context.sql.executeInsert("INSERT INTO tenant_resource VALUES ($tenantId,$resourceId,${"client-bdm.zip"},${"BDM"},${clientBDMZip.bytes})")
+        clientBDMZip.delete()
+        resourcesCount.put(tenantId, resourceId + 1)
     }
 
     private static Map<Long, Long> insertSequences(LinkedHashMap<Long, Long> resourcesCount, context) {
