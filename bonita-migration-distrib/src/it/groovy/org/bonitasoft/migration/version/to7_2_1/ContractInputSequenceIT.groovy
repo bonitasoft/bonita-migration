8d5e3228952abbd648818e6b1790b81c497e5127
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2016-07-26 16:30:59 +0200

    fix(migration): removing sequence could generate duplicate key exception (#70)
    * when merging sequence used for process and task contract input, new sequence value may have a lower value
     * when merging sequence used for archived process and task contract input, new sequence value may have a lower value
    
      Closes [BS-15360](https://bonitasoft.atlassian.net/browse/BS-15360)

---
+++ bonita-migration-distrib/src/it/groovy/org/bonitasoft/migration/version/to7_2_1/ContractInputSequenceIT.groovy
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2016 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ */
+package org.bonitasoft.migration.version.to7_2_1
+
+import org.bonitasoft.migration.DBUnitHelper
+import org.bonitasoft.migration.core.Logger
+import org.bonitasoft.migration.core.MigrationContext
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * @author Laurent Leseigneur
+ */
+class ContractInputSequenceIT extends Specification {
+
+    @Shared
+    Logger logger = Mock(Logger)
+
+    @Shared
+    MigrationContext migrationContext = new MigrationContext(logger: logger)
+
+    @Shared
+    DBUnitHelper dbUnitHelper = new DBUnitHelper(migrationContext)
+
+    def setup() {
+        migrationContext.setVersion("7.2.1")
+        dropTestTables()
+        dbUnitHelper.createTables("7_2_1/sequence", "sequence")
+    }
+
+    def cleanup() {
+        dropTestTables()
+    }
+
+    private String[] dropTestTables() {
+        dbUnitHelper.dropTables(["sequence"] as String[])
+    }
+
+    def "should update sequence value for contract input"() {
+        setup:
+        dbUnitHelper.context.sql.execute("INSERT INTO sequence (tenantid,id,nextid) VALUES(25, 10210, 16)")
+        dbUnitHelper.context.sql.execute("INSERT INTO sequence (tenantid,id,nextid) VALUES(25, 10220, 1)")
+
+        dbUnitHelper.context.sql.execute("INSERT INTO sequence (tenantid,id,nextid) VALUES(35, 10210, 13)")
+        dbUnitHelper.context.sql.execute("INSERT INTO sequence (tenantid,id,nextid) VALUES(35, 10220, 18)")
+
+        when:
+        new ContractInputSequence().execute(migrationContext)
+
+        then:
+        def sequences = dbUnitHelper.context.sql.rows("SELECT tenantid, id, nextid FROM sequence s WHERE s.tenantid IN(25,35) AND s.id IN (10210,10220) ORDER BY s.tenantid, s.id ")
+
+        sequences.size() == 2
+
+        sequences[0].tenantid == 25
+        sequences[0].id == 10210
+        sequences[0].nextid == 17
+
+        sequences[1].tenantid == 35
+        sequences[1].id == 10210
+        sequences[1].nextid == 19
+    }
+
+    def "should update sequence value for archived contract input"() {
+        setup:
+        dbUnitHelper.context.sql.execute("INSERT INTO sequence (tenantid,id,nextid) VALUES(12, 20210, 46)")
+        dbUnitHelper.context.sql.execute("INSERT INTO sequence (tenantid,id,nextid) VALUES(12, 20220, 79)")
+
+        dbUnitHelper.context.sql.execute("INSERT INTO sequence (tenantid,id,nextid) VALUES(16, 20210, 39)")
+        dbUnitHelper.context.sql.execute("INSERT INTO sequence (tenantid,id,nextid) VALUES(16, 20220, 23)")
+
+        when:
+        new ContractInputSequence().execute(migrationContext)
+
+        then:
+        def sequences = dbUnitHelper.context.sql.rows("SELECT tenantid, id, nextid FROM sequence s WHERE s.tenantid IN (12,16) AND s.id IN (20210,20220) ORDER BY s.tenantid, s.id ")
+
+        sequences.size() == 2
+
+        sequences[0].tenantid == 12
+        sequences[0].id == 20210
+        sequences[0].nextid == 80
+
+        sequences[1].tenantid == 16
+        sequences[1].id == 20210
+        sequences[1].nextid == 40
+
+    }
+
+}
