dca4fe00c005bbcf837cf15ecadad0cde39dad5b
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2016-07-26 16:30:59 +0200

    fix(migration): removing sequence could generate duplicate key exception (#70)
    * when merging sequence used for process and task contract input, new sequence value may have a lower value
     * when merging sequence used for archived process and task contract input, new sequence value may have a lower value
    
      Closes [BS-15360](https://bonitasoft.atlassian.net/browse/BS-15360)

--- bonita-migration-distrib/src/main/groovy/org/bonitasoft/migration/version/to7_2_1/ContractInputSequence.groovy
+++ bonita-migration-distrib/src/main/groovy/org/bonitasoft/migration/version/to7_2_1/ContractInputSequence.groovy
@@ -1,7 +1,9 @@
 package org.bonitasoft.migration.version.to7_2_1
 
+import groovy.sql.GroovyRowResult
 import org.bonitasoft.migration.core.MigrationContext
 import org.bonitasoft.migration.core.MigrationStep
+
 /**
  *
  * parameters where in bonita-home/engine-server/work/tenants/<tenantId>/processes/<process_id>/parameters.properties
@@ -11,13 +13,53 @@ import org.bonitasoft.migration.core.MigrationStep
 class ContractInputSequence extends MigrationStep {
 
 
+    public static
+    final String SQL_CHECK_SEQUENCES = "SELECT tenantid, id, nextid FROM sequence WHERE id in (10220,20220, 10210 ,20210 ) ORDER BY  tenantid, id "
+
     @Override
     def execute(MigrationContext context) {
-        //remove  10220 and 20220
-        context.sql.execute("DELETE FROM sequence WHERE id = ${10220} OR id = ${20220}")
+        checkSequenceValues(context)
 
+        context.sql.rows("""
+                    SELECT
+                        s.TENANTID,
+                        10210 AS id,
+                        MAX( s.NEXTID ) + 1 AS newValue
+                    FROM sequence s
+                    WHERE s.id IN(
+                        10210,
+                        10220
+                    )
+                    GROUP BY s.TENANTID
+                """).each {
+            context.sql.execute("UPDATE sequence set nextid= ? WHERE tenantid= ? and id = ?", it.newValue, it.tenantId, it.id)
+        }
+
+        context.sql.rows("""
+                    SELECT
+                        s.TENANTID,
+                        20210 AS id,
+                        MAX( s.NEXTID ) + 1 AS newValue
+                    FROM sequence s
+                    WHERE s.id IN(
+                        20210,
+                        20220
+                    )
+                    GROUP BY s.TENANTID
+                """).each {
+            context.sql.execute("UPDATE sequence set nextid= ? WHERE tenantid= ? and id = ?", it.newValue, it.tenantId, it.id)
+        }
+
+        context.sql.execute("DELETE FROM sequence WHERE id IN (10220,20220)")
+
+        checkSequenceValues(context)
     }
 
+    private List<GroovyRowResult> checkSequenceValues(MigrationContext context) {
+        context.sql.rows(SQL_CHECK_SEQUENCES).each {
+            context.logger.info "sequence value before migration: $it"
+        }
+    }
 
     @Override
     String getDescription() {
