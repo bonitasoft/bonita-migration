87a0b5ee713a95c732b68b5d8ff94d28bcc0b215
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2016-08-05 17:55:29 +0200

    fix(migration): remove unique constraint in  ref_biz_data tables (#73)
    * remove unique constraint on tables ref_biz_data_inst and arch_ref_biz_data_inst
    * improve mysql cleanDb plugin task to make it work on empty mysql docker container
    
    Closes [BS-15662](https://bonitasoft.atlassian.net/browse/BS-15662)

---
+++ bonita-migration-distrib/src/it/resources/sql/v7_3_2/bizdata/mysql.sql
@@ -0,0 +1,32 @@
+CREATE TABLE ref_biz_data_inst (
+	tenantid BIGINT NOT NULL,
+  	id BIGINT NOT NULL,
+  	kind VARCHAR(15) NOT NULL,
+  	name VARCHAR(255) NOT NULL,
+  	proc_inst_id BIGINT,
+  	fn_inst_id BIGINT,
+  	data_id BIGINT,
+  	data_classname VARCHAR(255) NOT NULL
+) ENGINE = INNODB;
+
+CREATE INDEX idx_biz_data_inst1 ON ref_biz_data_inst (tenantid, proc_inst_id);
+CREATE INDEX idx_biz_data_inst2 ON ref_biz_data_inst (tenantid, fn_inst_id);
+ALTER TABLE ref_biz_data_inst ADD CONSTRAINT pk_ref_biz_data_inst PRIMARY KEY (tenantid, id);
+ALTER TABLE ref_biz_data_inst ADD CONSTRAINT uk_ref_biz_data_inst UNIQUE (name, proc_inst_id, fn_inst_id, tenantid);
+
+
+CREATE TABLE arch_ref_biz_data_inst (
+	tenantid BIGINT NOT NULL,
+  	id BIGINT NOT NULL,
+  	kind VARCHAR(15) NOT NULL,
+  	name VARCHAR(255) NOT NULL,
+  	orig_proc_inst_id BIGINT,
+  	orig_fn_inst_id BIGINT,
+  	data_id BIGINT,
+  	data_classname VARCHAR(255) NOT NULL
+);
+CREATE INDEX idx_arch_biz_data_inst1 ON arch_ref_biz_data_inst (tenantid, orig_proc_inst_id);
+CREATE INDEX idx_arch_biz_data_inst2 ON arch_ref_biz_data_inst (tenantid, orig_fn_inst_id);
+ALTER TABLE arch_ref_biz_data_inst ADD CONSTRAINT pk_arch_ref_biz_data_inst PRIMARY KEY (tenantid, id);
+ALTER TABLE arch_ref_biz_data_inst ADD CONSTRAINT uk_arch_ref_biz_data_inst UNIQUE (name, orig_proc_inst_id, orig_fn_inst_id, tenantid);
+
