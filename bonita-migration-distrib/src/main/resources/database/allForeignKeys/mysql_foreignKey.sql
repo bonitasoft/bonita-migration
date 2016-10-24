9cdb2160009f9787fa1c95ce2d67f98bd6057ae6
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2016-10-24 17:40:18 +0200

    fix(quartz): foreign keys name (#83)
    * use a generic foreign key utility method
    
    Closes [BS-15879](https://bonitasoft.atlassian.net/browse/BS-15879)

---
+++ bonita-migration-distrib/src/main/resources/database/allForeignKeys/mysql_foreignKey.sql
@@ -0,0 +1,12 @@
+SELECT
+    c.TABLE_NAME,
+    c.CONSTRAINT_NAME
+FROM
+    information_schema.TABLE_CONSTRAINTS c
+WHERE
+    c.CONSTRAINT_SCHEMA =(
+        SELECT
+            DATABASE()
+    )
+    AND LOWER( c.TABLE_NAME ) = LOWER( ? )
+    AND c.CONSTRAINT_TYPE = 'FOREIGN KEY'
