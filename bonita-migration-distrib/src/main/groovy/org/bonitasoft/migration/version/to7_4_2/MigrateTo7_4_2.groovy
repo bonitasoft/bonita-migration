4f88e19f8065b0f02922167150d35ec1c864df40
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2017-03-23 13:54:33 +0100

    fix(7.4.0 step) add missing properties (#113)
    * add new entries in resources-permissions-mapping.properties
    
    Closes [BS-16196](https://bonitasoft.atlassian.net/browse/BS-16196)

--- bonita-migration-distrib/src/main/groovy/org/bonitasoft/migration/version/to7_4_2/MigrateTo7_4_2.groovy
+++ bonita-migration-distrib/src/main/groovy/org/bonitasoft/migration/version/to7_4_2/MigrateTo7_4_2.groovy
@@ -15,6 +15,7 @@ package org.bonitasoft.migration.version.to7_4_2
 
 import org.bonitasoft.migration.core.MigrationStep
 import org.bonitasoft.migration.core.VersionMigration
+import org.bonitasoft.migration.version.to7_4_0.UpdatePermissionMappingProperties
 
 /**
  */
@@ -24,7 +25,8 @@ class MigrateTo7_4_2 extends VersionMigration {
     def List<MigrationStep> getMigrationSteps() {
         //keep one line per step to avoid false-positive merge conflict
         return [
-                new AddManagerInvolvedConfiguration()
+                new AddManagerInvolvedConfiguration(),
+                new UpdatePermissionMappingProperties()
         ]
     }
 }
