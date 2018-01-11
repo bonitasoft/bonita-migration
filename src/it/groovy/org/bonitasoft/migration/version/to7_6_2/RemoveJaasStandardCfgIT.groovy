2cbf3ce45b20b2c109d2838f57335ed88466973f
abirembaut <anthony.birembaut@gmail.com>
2018-01-11 11:48:35 +0100

    fix(config): remove unused JAAS config file (#163)
    Add migration to remove extra file jaas-standard.cfg in 7.6.2
    
    Covers [BS-17736](https://bonitasoft.atlassian.net/browse/BS-17736)

---
+++ src/it/groovy/org/bonitasoft/migration/version/to7_6_2/RemoveJaasStandardCfgIT.groovy
@@ -0,0 +1,53 @@
+package org.bonitasoft.migration.version.to7_6_2
+
+import org.bonitasoft.migration.DBUnitHelper
+import org.bonitasoft.migration.core.Logger
+import org.bonitasoft.migration.core.MigrationContext
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * @author Anthony Birembaut
+ */
+class RemoveJaasStandardCfgIT extends Specification {
+
+    @Shared
+    Logger logger = new Logger()
+
+
+    @Shared
+    MigrationContext migrationContext = new MigrationContext(logger: logger)
+
+    @Shared
+    DBUnitHelper dbUnitHelper = new DBUnitHelper(migrationContext)
+
+    private RemoveJaasStandardCfg migrationStep = new RemoveJaasStandardCfg()
+
+    def setup() {
+        dropTestTables()
+        dbUnitHelper.createTables("7_4_2", "configuration")
+    }
+
+    def cleanup() {
+        dropTestTables()
+    }
+
+    private String[] dropTestTables() {
+        dbUnitHelper.dropTables(["configuration"] as String[])
+    }
+
+
+    def "should remove the jaas-standard.cfg file from PLATFORM_PORTAL"() {
+        given:
+        migrationContext.sql.executeInsert("insert into configuration(tenant_id, content_type, resource_name, resource_content) values (?,?,?,?)",
+                0L, "PLATFORM_PORTAL", 'jaas-standard.cfg', 'somebytes'.bytes)
+        assert 1 == dbUnitHelper.countConfigFileWithName("PLATFORM_PORTAL", 'jaas-standard.cfg')
+
+        when:
+        migrationStep.execute(migrationContext)
+
+        then:
+        0 == dbUnitHelper.countConfigFileWithName("PLATFORM_PORTAL", 'jaas-standard.cfg')
+
+    }
+}
