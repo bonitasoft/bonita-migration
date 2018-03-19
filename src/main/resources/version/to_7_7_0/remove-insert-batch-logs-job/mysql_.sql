4b602397921a752eacf960d3b7e35875c538a154
Baptiste Mesta <baptiste.mesta@gmail.com>
2018-03-19 17:19:09 +0100

    feat(logs): remove InsertBatchLogsJob (#171)
    It is not used anymore in the engine.

---
+++ src/main/resources/version/to_7_7_0/remove-insert-batch-logs-job/mysql_.sql
@@ -0,0 +1,4 @@
+delete from QRTZ_CRON_TRIGGERS where TRIGGER_NAME in (select t.TRIGGER_NAME from QRTZ_TRIGGERS t where t.JOB_NAME = 'InsertBatchLogsJob');
+delete from QRTZ_FIRED_TRIGGERS where TRIGGER_NAME in (select t.TRIGGER_NAME from QRTZ_TRIGGERS t where t.JOB_NAME = 'InsertBatchLogsJob');
+delete from QRTZ_TRIGGERS where JOB_NAME = 'InsertBatchLogsJob';
+delete from QRTZ_JOB_DETAILS where JOB_NAME = 'InsertBatchLogsJob';
