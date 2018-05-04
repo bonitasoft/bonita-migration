435ba2afc460bd4ea170b5dcb817cd00acccb93a
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2018-05-04 15:32:07 +0200

    chore(gradle): better split tests and migration plugins
    Move IT configuration out of the distrib plugin
    The distibution plugin is for global distribution tasks not for tests,
    so move all integration test configuration in the migration plugin with
    the migration tests configuration for consistency.
    
    Improve description of some tasks
    
    Code sharing between plugins
      - move all versions related methods to the VersionUtils class.
      - move loadProperties method in a utils class, this removes duplication
    
    Simplify dependencies
      - clean dependencies
      - remove the unused deployerJars configuration and dependencies
      - remove the classpath database config
      The classpath field is only used internally when loading jdbc drivers.
      Making it public is confusing and implied duplication in community and
      subscription build scripts.
      As we have a more direct way to retrieve jdbc drivers, use it and remove
      the field.
      - centralize jdbc drivers dependencies setting to avoid duplication
      - distribution: limit scope to runtime for mysql and postgre as no code
      rely on them for compilation
    
    CleanDbTask
      - remove duplication when checking credentials + fix logs
      - remove useless cast in clean postgres
    
    Gradle extension that hold db information
      - rename it as it is not only for clean db task but for all db operations
      - remove unused imports and clean fields declaration
    
    Rename the task that prepares migration tests
    This clarifies that it is for migration tests, and not for unit or
    integration tests.

---
+++ buildSrc/src/main/groovy/org/bonitasoft/migration/plugin/db/JdbcDriverDependencies.groovy
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2018 Bonitasoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+
+package org.bonitasoft.migration.plugin.db
+
+class JdbcDriverDependencies {
+
+    final static String mysql = 'mysql:mysql-connector-java:5.1.26'
+    final static String oracle = 'com.oracle:ojdbc:6'
+    final static String postgres = 'org.postgresql:postgresql:9.3-1102-jdbc41'
+    final static String sqlserver = 'com.microsoft.jdbc:sqlserver:6.0.8112.100_41'
+
+}
