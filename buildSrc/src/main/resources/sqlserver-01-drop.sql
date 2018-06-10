d824e5f099d007b02fe4d853f199310344af3e70
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2018-06-10 15:12:21 +0200

    chore(sqlserver): reduce errors during the drop database
    Previous implementation often failed to put the database in an offline state
    (timeout occured). So it was then impossible to perform any operations on it.
    The workaround was to stop the whole SQL Server to release locks to be
    able to drop the database.
    
    The new implementation now drops all existing connections then deleted
    the database. This highly reduces the number of failures.

--- buildSrc/src/main/resources/sqlserver-01-drop.sql
+++ buildSrc/src/main/resources/sqlserver-01-drop.sql
@@ -1,13 +1,32 @@
--- Drop database if exists
-IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'@sqlserver.db.name@')
+-- #############################################################################
+-- Before dropping users and databases, we removed sessions associated or connected to them (see -- See https://stackoverflow.com/a/7201350)
+-- #############################################################################
+
+
+-- Ensure we use master database
+USE master;
+GO
+
+
+-- Drop login if exists
+-- This will prevent any new connections using it
+IF EXISTS (SELECT name FROM sys.server_principals WHERE name = '@sqlserver.connection.username@')
 BEGIN
-  ALTER DATABASE [@sqlserver.db.name@] SET OFFLINE WITH ROLLBACK IMMEDIATE;
-  ALTER DATABASE [@sqlserver.db.name@] SET ONLINE;
-  DROP DATABASE [@sqlserver.db.name@];
+  DROP LOGIN @sqlserver.connection.username@
 END
 GO
 
+-- Drop all sessions on all databases that were using this login
+DECLARE @kill_all varchar(max) = '';  -- use max to ensure the kill won't fail because we have not allow enough resources
+SELECT @kill_all = @kill_all + 'BEGIN TRY KILL ' + CONVERT(varchar(5), session_id) + ' END TRY BEGIN CATCH END CATCH ;' FROM sys.dm_exec_sessions
+WHERE login_name = '@sqlserver.connection.username@'
+
+EXEC(@kill_all);
+GO
+
+
 -- Drop user in the master database (used for xa) if exists
+-- There should not be any sessions associated to this user as it uses the login (we previously removed it and all its associated running sessions)
 IF EXISTS (SELECT name FROM sys.database_principals WHERE name = '@sqlserver.connection.username@')
 BEGIN
   DROP USER @sqlserver.connection.username@;
@@ -15,9 +34,18 @@ END
 GO
 
 
--- Drop login if exists
-IF EXISTS (SELECT name FROM sys.server_principals WHERE name = '@sqlserver.connection.username@')
+-- Kill all sessions connected to the database
+DECLARE @kill varchar(max) = '';  -- use max to ensure the kill won't fail because we have not allow enough resources
+SELECT @kill = @kill + 'BEGIN TRY KILL ' + CONVERT(varchar(5), session_id) + ' END TRY BEGIN CATCH END CATCH ;' FROM sys.dm_exec_sessions
+WHERE database_id  = db_id('@sqlserver.db.name@')
+
+EXEC(@kill);
+GO
+
+
+-- Drop database if exists (there should be no error as we have kill all sessions previously connected to it)
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'@sqlserver.db.name@')
 BEGIN
-  DROP LOGIN @sqlserver.connection.username@
+  DROP DATABASE [@sqlserver.db.name@];
 END
 GO
