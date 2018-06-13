5cfe658373824468794efc284ab93decb6e52871
abirembaut <anthony.birembaut@gmail.com>
2018-06-13 14:11:26 +0200

    fix(contract input migration) 7.7.0 contract input migration null value (#196)
    Covers [BS-18435](https://bonitasoft.atlassian.net/browse/BS-18435)

--- src/it/groovy/org/bonitasoft/migration/version/to7_7_0/ChangeContractInputSerializationIT.groovy
+++ src/it/groovy/org/bonitasoft/migration/version/to7_7_0/ChangeContractInputSerializationIT.groovy
@@ -1,13 +1,13 @@
 package org.bonitasoft.migration.version.to7_7_0
 
-import static org.bonitasoft.migration.core.MigrationStep.DBVendor.ORACLE
-
 import org.bonitasoft.migration.DBUnitHelper
 import org.bonitasoft.migration.core.Logger
 import org.bonitasoft.migration.core.MigrationContext
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import static org.bonitasoft.migration.core.MigrationStep.DBVendor.ORACLE
 
 class ChangeContractInputSerializationIT extends Specification {
 
@@ -45,6 +45,10 @@ insert into contract_data(tenantId , id, kind, scopeId, name, val)
 values (1, 1, 'PROCESS', 1000, 'myInput', $content)
 """)
         migrationContext.sql.execute("""
+insert into contract_data(tenantId , id, kind, scopeId, name, val)
+values (1, 2, 'PROCESS', 1001, 'myInputNull', NULL)
+""")
+        migrationContext.sql.execute("""
 insert into arch_contract_data(tenantId , sourceobjectid, archiveDate, id, kind, scopeId, name, val)
 values (1, 1, 1, 1, 'PROCESS', 1000, 'myInput', $content)
 """)
@@ -52,8 +56,12 @@ values (1, 1, 1, 1, 'PROCESS', 1000, 'myInput', $content)
         migrationStep.execute(migrationContext)
 
         then: 'contract data is migrated'
-        def contract_data = migrationContext.sql.firstRow("SELECT * FROM contract_data")
+        def contract_data = migrationContext.sql.firstRow("SELECT * FROM contract_data WHERE name='myInput'")
         getMigratedValue(contract_data.val) == "myValue"
+
+        then: 'contract data with NULL value is migrated'
+        def null_contract_data = migrationContext.sql.firstRow("SELECT * FROM contract_data WHERE name='myInputNull'")
+        getMigratedValue(null_contract_data.val) == null
 
         and: 'archived contract data is migrated'
         def arch_contract_data = migrationContext.sql.firstRow("SELECT * FROM  arch_contract_data")
@@ -70,7 +78,7 @@ values (1, 1, 1, 1, 'PROCESS', 1000, 'myInput', $content)
 
     private String getMigratedValue(def value) {
         def migratedValue = value
-        if (ORACLE.equals(migrationContext.dbVendor)) {
+        if (value != null && ORACLE.equals(migrationContext.dbVendor)) {
             migratedValue = migrationContext.databaseHelper.getClobContent(value)
         }
         migratedValue
