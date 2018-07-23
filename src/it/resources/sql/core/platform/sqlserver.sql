395647ccfbf8d35ad192129b84ccdae36e574670
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2018-07-23 15:21:23 +0200

    fix(edition): prevent to run Community edition on Bonita Subscription (#209)
    There is no automatic edition check done when the current Bonita version
    is before 7.1.0
    
    The checked column 'information' does not exist prior to this version,
    so in this case, we log a warning and let the end user decide whether or
    not he wants to continue.
    
    Covers [BS-18581](https://bonitasoft.atlassian.net/browse/BS-18581)

---
+++ src/it/resources/sql/core/platform/sqlserver.sql
@@ -0,0 +1,11 @@
+CREATE TABLE platform (
+  id NUMERIC(19, 0) NOT NULL,
+  version NVARCHAR(50) NOT NULL,
+  previousVersion NVARCHAR(50) NOT NULL,
+  initialVersion NVARCHAR(50) NOT NULL,
+  created NUMERIC(19, 0) NOT NULL,
+  createdBy NVARCHAR(50) NOT NULL,
+  information NVARCHAR(MAX),
+  PRIMARY KEY (id)
+)
+GO
