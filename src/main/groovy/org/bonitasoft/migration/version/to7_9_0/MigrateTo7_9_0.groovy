ab74eea1337281cb9424c60d28b5774f4b56372a
danila_mazour <danila.mazour@bonitasoft.com>
2019-04-17 10:23:46 +0200

    feat(connectors): add migration warning on 7.9.0 version (#244)
    Relates to [BR-1](https://bonitasoft.atlassian.net/browse/BR-1)
    Relates to [BR-86](https://bonitasoft.atlassian.net/browse/BR-86)

--- src/main/groovy/org/bonitasoft/migration/version/to7_9_0/MigrateTo7_9_0.groovy
+++ src/main/groovy/org/bonitasoft/migration/version/to7_9_0/MigrateTo7_9_0.groovy
@@ -13,6 +13,7 @@
  **/
 package org.bonitasoft.migration.version.to7_9_0
 
+import org.bonitasoft.migration.core.MigrationContext
 import org.bonitasoft.migration.core.MigrationStep
 import org.bonitasoft.migration.core.VersionMigration
 
@@ -20,6 +21,23 @@ import org.bonitasoft.migration.core.VersionMigration
  * @author Danila Mazour
  */
 class MigrateTo7_9_0 extends VersionMigration {
+
+    public final static String[] CONNECTOR_MIGRATION_MESSAGE =
+            ["The Bonita platform, from 7.9.0 upwards, supports running on java 11.",
+             "In order to make the process of migrating a Bonita Platform to Java 11 as smooth as possible, the 7.9.0 migration step will try to migrate the",
+             "CMIS, Webservice & Email",
+             "connectors of all the deployed processes, along with their dependencies, to a version that supports java 11.",
+             "The migration tool will try to do the connector migration on a best effort basis. ",
+             "If the dependencies of these connectors are used in other connectors, the corresponding connectors will not be migrated.",
+             "It means that, potentially, some of those connectors will not be migrated and will have to be updated manually after the migration tool completes",
+             "If some Groovy scripts use the same dependencies as the old connectors' versions, they might be broken after migration, requiring a manual update",
+             "On a separated note, the SAP connector will not work anymore on Java 11. If you are using it, and plan to migrate to Java 11, it might be best to postpone your migration, and contact Bonita support",
+             "To know more, check the documentation (http://www.bonitasoft.com/bos_redirect.php?bos_redirect_id=686&bos_redirect_product=bos&bos_redirect_major_version=7.9)."]
+
+    @Override
+    String[] getPreMigrationWarnings(MigrationContext context) {
+        return CONNECTOR_MIGRATION_MESSAGE
+    }
 
     @Override
     List<MigrationStep> getMigrationSteps() {
