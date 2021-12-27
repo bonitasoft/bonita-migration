cd2d846497ff16f8b31e4bf8e17f9471e5cbe9df
Dumitru Corini <dumitru.corini@bonitasoft.com>
2021-12-27 09:37:21 +0200

    feat(7.14): Remove "hidden" field from pages (#350)
    * feat(7.14): Remove "hidden" field from pages
    
    covers [RUNTIME-765](https://bonitasoft.atlassian.net/browse/RUNTIME-765)

---
+++ src/it/groovy/org/bonitasoft/migration/version/to7_14_0/RemoveHiddenFieldFromPageIT.groovy
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2021 Bonitasoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.migration.version.to7_14_0
+
+import org.bonitasoft.migration.DBUnitHelper
+import org.bonitasoft.migration.core.MigrationContext
+import org.bonitasoft.migration.version.to7_13_0.CreateNewPages
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * @author Dumitru Corini
+ */
+class RemoveHiddenFieldFromPageIT extends Specification {
+
+    @Shared
+    DBUnitHelper dbUnitHelper = DBUnitHelper.getInstance()
+    @Shared
+    MigrationContext migrationContext = dbUnitHelper.context
+
+    private RemoveHiddenFieldFromPages migrationStep = new RemoveHiddenFieldFromPages()
+
+    def setup() {
+        dropTestTables()
+        dbUnitHelper.createTables("7_13_0/pages")
+    }
+
+    def cleanup() {
+        dropTestTables()
+    }
+
+    private String[] dropTestTables() {
+        dbUnitHelper.dropTables(["page", "sequence", "tenant"] as String[])
+    }
+
+
+    def "should remove hidden field from pages"() {
+        given:
+        assert migrationContext.databaseHelper.hasColumnOnTable("page", "hidden")
+
+        when:
+        migrationStep.execute(migrationContext)
+
+        then:
+        !migrationContext.databaseHelper.hasColumnOnTable("page", "hidden")
+    }
+}
---
+++ src/test/groovy/org/bonitasoft/migration/version/to7_14_0/MigrateTo7_14_0Test.groovy
@@ -0,0 +1,28 @@
+package org.bonitasoft.migration.version.to7_14_0
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * @author Dumitru Corini
+ */
+class MigrateTo7_14_0Test extends Specification {
+
+    @Unroll
+    def "should migration to 7.14.0 include step '#stepName'"(def stepName) {
+        given:
+        def migrateTo = new MigrateTo7_14_0()
+
+        expect:
+        def steps = migrateTo.migrationSteps
+        steps.collect {
+            it.class.getSimpleName()
+        }.contains(stepName)
+
+        where:
+        stepName << [
+                "RemoveHiddenFieldFromPages"
+        ]
+
+    }
+}
