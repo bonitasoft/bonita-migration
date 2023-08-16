fa358c441a7aa82d67aa3e2435a5363d891472cf
Haroun El Alami <haroun.dev@gmail.com>
2023-08-16 15:36:06 +0200

    fix(file upload): add missing temporary_content sequence (#413)
    closes to [DEV-416](https://bonitasoft.atlassian.net/browse/DEV-416)

--- src/integrationTest/resources/sql/v9_0_0/postgres.sql
+++ src/integrationTest/resources/sql/v9_0_0/postgres.sql
@@ -1,3 +1,10 @@
+CREATE TABLE sequence (
+  tenantid INT8 NOT NULL,
+  id INT8 NOT NULL,
+  nextid INT8 NOT NULL,
+  PRIMARY KEY (tenantid, id)
+);
+
 CREATE TABLE platform (
   id INT8 NOT NULL,
   version VARCHAR(50) NOT NULL,
