c9b84aa52ee58a31edea696c85f8b9413c308620
Romain Bioteau <romain.bioteau@bonitasoft.com>
2024-04-11 17:35:36 +0200

    feat(messages): add warning for sanitizer filter (#442)
    ---------
    Co-authored-by: emmanuel.duchastenier@bonitasoft.com <emmanuel.duchastenier@bonitasoft.com>

--- src/test/groovy/org/bonitasoft/update/version/to10_0_0/UpdateTo10_0_0Test.groovy
+++ src/test/groovy/org/bonitasoft/update/version/to10_0_0/UpdateTo10_0_0Test.groovy
@@ -1,6 +1,18 @@
+/**
+ * Copyright (C) 2024 Bonitasoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
 package org.bonitasoft.update.version.to10_0_0
 
-import org.bonitasoft.update.core.UpdateContext
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -10,8 +22,6 @@ import spock.lang.Unroll
 class UpdateTo10_0_0Test extends Specification {
 
     @Unroll
-
-
     def "should 10.0.0 preUpdateWarnings warn about Java 17 & wordSearchExclusionMapping & dynamic authorization"() {
         setup:
         def version = Spy(UpdateTo10_0_0.class)
@@ -25,6 +35,7 @@ class UpdateTo10_0_0Test extends Specification {
         warnings.any {
             it.contains("Java 17")
             it.contains("wordSearchExclusionMapping")
+            it.contains("CVE-2024-26542")
             it.contains("dynamic REST API authorizations")
         }
     }
@@ -44,6 +55,7 @@ class UpdateTo10_0_0Test extends Specification {
         stepName << [
                 "RemoveEnableWordSearchConfig",
                 "AddEnableDynamicCheckConfig"
+                , "AddSecuritySanitizerConfig"
         ]
     }
 
